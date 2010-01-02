1f7b73a2c83db4db0cad79f275d4a615688fa13f
Fredrik Tolf <fredrik@dolda2000.com>
2010-01-02 19:34:39 +0100

    Added a library class loader for use with javac.

---
+++ src/haven/resutil/CompilerClassLoader.java
@@ -0,0 +1,29 @@
+package haven.resutil;
+
+import haven.*;
+
+public class CompilerClassLoader extends ClassLoader {
+    private Resource[] useres;
+
+    static {
+	Config.nopreload = true;
+    }
+    
+    public CompilerClassLoader(ClassLoader parent) {
+	super(parent);
+	String[] useresnm = Utils.getprop("haven.resutil.classloader.useres", null).split(":");
+	this.useres = new Resource[useresnm.length];
+	for(int i = 0; i < useresnm.length; i++)
+	    this.useres[i] = Resource.load(useresnm[i]);
+    }
+    
+    public Class<?> findClass(String name) throws ClassNotFoundException {
+	for(Resource res : useres) {
+	    res.loadwait();
+	    try {
+		return(res.layer(Resource.CodeEntry.class).loader.loadClass(name));
+	    } catch(ClassNotFoundException e) {}
+	}
+	throw(new ClassNotFoundException(name + " was not found in any of the requested resources."));
+    }
+}
