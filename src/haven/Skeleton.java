a29b67c7b6e7c8ba607d693421bfc155eb37d3e6
Fredrik Tolf <fredrik@dolda2000.com>
2010-11-09 06:20:23 +0100

    Load and represent skeletons more nicely.

--- src/haven/Skeleton.java
+++ src/haven/Skeleton.java
@@ -29,10 +29,43 @@ package haven;
 import java.util.*;
 
 public class Skeleton {
-    public Map<String, Bone> bones = new HashMap<String, Bone>();
-    public Bone[] blist;
+    public final Map<String, Bone> bones = new HashMap<String, Bone>();
+    public final Bone[] blist;
+
+    public Skeleton(Collection<Bone> bones) {
+	Set<Bone> bset = new HashSet<Bone>(bones);
+	blist = new Bone[bones.size()];
+	int idx = 0;
+	for(Bone b : bones)
+	    this.bones.put(b.name, b);
+	while(!bset.isEmpty()) {
+	    boolean f = false;
+	    for(Iterator<Bone> i = bset.iterator(); i.hasNext();) {
+		Bone b = i.next();
+		boolean has;
+		if(b.parent == null) {
+		    has = true;
+		} else {
+		    has = false;
+		    for(Bone p : blist) {
+			if(p == b.parent) {
+			    has = true;
+			    break;
+			}
+		    }
+		}
+		if(has) {
+		    blist[b.idx = idx++] = b;
+		    i.remove();
+		    f = true;
+		}
+	    }
+	    if(!f)
+		throw(new RuntimeException("Cyclical bone hierarchy"));
+	}
+    }
     
-    public class Bone {
+    public static class Bone {
 	public String name;
 	public Coord3f ipos, irax;
 	public float irang;
@@ -52,35 +85,33 @@ public class Skeleton {
 	
 	public Res(Resource res, byte[] buf) {
 	    res.super();
-	    s = new Skeleton();
+	    Map<String, Bone> bones = new HashMap<String, Bone>();
 	    Map<Bone, String> pm = new HashMap<Bone, String>();
 	    int[] off = {0};
 	    while(off[0] < buf.length) {
 		String bnm = Utils.strd(buf, off);
 		Coord3f pos = new Coord3f((float)Utils.floatd(buf, off[0]), (float)Utils.floatd(buf, off[0] + 5), (float)Utils.floatd(buf, off[0] + 10));
 		off[0] += 15;
-		Coord3f rax = new Coord3f((float)Utils.floatd(buf, off[0]), (float)Utils.floatd(buf, off[0] + 5), (float)Utils.floatd(buf, off[0] + 10));
+		Coord3f rax = new Coord3f((float)Utils.floatd(buf, off[0]), (float)Utils.floatd(buf, off[0] + 5), (float)Utils.floatd(buf, off[0] + 10)).norm();
 		off[0] += 15;
 		float rang = (float)Utils.floatd(buf, off[0]);
 		off[0] += 5;
 		String bp = Utils.strd(buf, off);
-		Bone b = s.new Bone(bnm, pos, rax, rang);
-		if(s.bones.put(bnm, b) != null)
-		    throw(new Resource.LoadException("Duplicate bone name: " + bnm, getres()));
+		Bone b = new Bone(bnm, pos, rax, rang);
+		if(bones.put(bnm, b) != null)
+		    throw(new RuntimeException("Duplicate bone name: " + b.name));
 		pm.put(b, bp);
 	    }
-	    s.blist = new Bone[s.bones.size()];
-	    int idx = 0;
-	    for(Bone b : s.bones.values()) {
-		s.blist[b.idx = idx++] = b;
+	    for(Bone b : bones.values()) {
 		String bp = pm.get(b);
 		if(bp.length() == 0) {
 		    b.parent = null;
 		} else {
-		    if((b.parent = s.bones.get(bp)) == null)
+		    if((b.parent = bones.get(bp)) == null)
 			throw(new Resource.LoadException("Parent bone " + bp + " not found for " + b.name, getres()));
 		}
 	    }
+	    s = new Skeleton(bones.values());
 	}
 	
 	public void init() {}
