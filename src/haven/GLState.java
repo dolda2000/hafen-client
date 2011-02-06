b9dd6e9f140e0d0ead70ed1a423968f37ba815cc
Fredrik Tolf <fredrik@dolda2000.com>
2011-02-06 07:31:25 +0100

    Only call reapply if using programmatic rendering.

--- src/haven/GLState.java
+++ src/haven/GLState.java
@@ -383,7 +383,7 @@ public abstract class GLState {
 		    cur.states[i].applyto(g, next.states[i]);
 		    GLState cs = cur.states[i];
 		    (cur.states[i] = next.states[i]).applyfrom(g, cs);
-		} else if(dirty && (shaders[i] != null)) {
+		} else if((prog != null) && dirty && (shaders[i] != null)) {
 		    cur.states[i].reapply(g);
 		}
 	    }
