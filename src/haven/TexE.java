66e13e5762bff19f97906caa6e890c43c3aa5bec
Fredrik Tolf <fredrik@dolda2000.com>
2012-02-12 06:04:12 +0100

    2D-render TexE:s upside-down by default.
    Or upside-up from OpenGL's point of view.

--- src/haven/TexE.java
+++ src/haven/TexE.java
@@ -30,16 +30,29 @@ import javax.media.opengl.*;
 
 public class TexE extends TexGL {
     public final int ifmt, dfmt, dtype;
+    public boolean invert;
     
-    public TexE(Coord sz, int ifmt, int dfmt, int dtype) {
+    public TexE(Coord sz, int ifmt, int dfmt, int dtype, boolean invert) {
 	super(sz, sz);
 	this.ifmt = ifmt;
 	this.dfmt = dfmt;
 	this.dtype = dtype;
+	this.invert = invert;
+    }
+    
+    public TexE(Coord sz, int ifmt, int dfmt, int dtype) {
+	this(sz, ifmt, dfmt, dtype, true);
     }
     
     protected void fill(GOut g) {
 	GL gl = g.gl;
 	gl.glTexImage2D(GL.GL_TEXTURE_2D, 0, ifmt, tdim.x, tdim.y, 0, dfmt, dtype, null);
+    }
+
+    public void render(GOut g, Coord c, Coord ul, Coord br, Coord sz) {
+	if(invert)
+	    super.render(g, c, new Coord(ul.x, br.y), new Coord(br.x, ul.y), sz);
+	else
+	    super.render(g, c, ul, br, sz);
     }
 }
