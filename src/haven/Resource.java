423ca3ca5e906c610a4f45feef4265605dbff44a
Fredrik Tolf <fredrik@dolda2000.com>
2012-07-21 03:26:30 +0200

    Handle nested resource loading version mismatches.
    But see comment.

--- src/haven/Resource.java
+++ src/haven/Resource.java
@@ -137,7 +137,13 @@ public class Resource implements Comparable<Resource>, Prioritized, Serializable
 			res = null;
 			cache.remove(name);
 		    } else if(res.ver > ver) {
-			throw(new RuntimeException(String.format("Weird version number on %s (%d > %d), loaded from %s", res.name, res.ver, ver, res.source)));
+			/* Throw LoadException rather than
+			 * RuntimeException here, to make sure
+			 * obsolete resources doing nested loading get
+			 * properly handled. This could be the wrong
+			 * way of going about it, however; I'm not
+			 * sure. */
+			throw(new LoadException(String.format("Weird version number on %s (%d > %d), loaded from %s", res.name, res.ver, ver, res.source), res));
 		    }
 		} else if(ver == -1) {
 		    if(res.error != null) {
