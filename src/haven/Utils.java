ae7e95ecfd658d7b9840ee02e099bcdb0b37d6b9
Fredrik Tolf <fredrik@dolda2000.com>
2012-09-14 06:23:03 +0200

    Added some fancy 2D graphics.

--- src/haven/Utils.java
+++ src/haven/Utils.java
@@ -624,7 +624,113 @@ public class Utils {
 	}
 	return(ret);
     }
-    
+
+    public static WritableRaster alpharaster(Coord sz) {
+	return(Raster.createInterleavedRaster(DataBuffer.TYPE_BYTE, sz.x, sz.y, 1, null));
+    }
+
+    public static void imggrow(WritableRaster img, int rad) {
+	int h = img.getHeight(), w = img.getWidth();
+	int[] buf = new int[w * h];
+	int o = 0;
+	for(int y = 0; y < h; y++) {
+	    for(int x = 0; x < w; x++) {
+		int m = 0;
+		int u = Math.max(0, y - rad), b = Math.min(h - 1, y + rad);
+		int l = Math.max(0, x - rad), r = Math.min(w - 1, x + rad);
+		for(int y2 = u; y2 <= b; y2++) {
+		    for(int x2 = l; x2 <= r; x2++) {
+			m = Math.max(m, img.getSample(x2, y2, 0));
+		    }
+		}
+		buf[o++] = m;
+	    }
+	}
+	img.setSamples(0, 0, w, h, 0, buf);
+    }
+
+    public static void imgblur(WritableRaster img, int rad, double var) {
+	int h = img.getHeight(), w = img.getWidth();
+	double[] gk = new double[(rad * 2) + 1];
+	for(int i = 0; i <= rad; i++)
+	    gk[rad + i] = gk[rad - i] = Math.exp(-0.5 * Math.pow(i / var, 2.0));
+	double s = 0;
+	for(double cw : gk) s += cw;
+	s = 1.0 / s;
+	for(int i = 0; i <= rad * 2; i++)
+	    gk[i] *= s;
+	int[] buf = new int[w * h];
+	for(int band = 0; band < img.getNumBands(); band++) {
+	    int o;
+	    o = 0;
+	    for(int y = 0; y < h; y++) {
+		for(int x = 0; x < w; x++) {
+		    double v = 0;
+		    int l = Math.max(0, x - rad), r = Math.min(w - 1, x + rad);
+		    for(int x2 = l, ks = l - (x - rad); x2 <= r; x2++, ks++)
+			v += img.getSample(x2, y, band) * gk[ks];
+		    buf[o++] = (int)v;
+		}
+	    }
+	    img.setSamples(0, 0, w, h, band, buf);
+	    o = 0;
+	    for(int y = 0; y < h; y++) {
+		for(int x = 0; x < w; x++) {
+		    double v = 0;
+		    int u = Math.max(0, y - rad), b = Math.min(h - 1, y + rad);
+		    for(int y2 = u, ks = u - (y - rad); y2 <= b; y2++, ks++)
+			v += img.getSample(x, y2, band) * gk[ks];
+		    buf[o++] = (int)v;
+		}
+	    }
+	    img.setSamples(0, 0, w, h, band, buf);
+	}
+    }
+
+    public static void alphadraw(WritableRaster dst, Raster alpha, Coord ul, Color col) {
+	int r = col.getRed(), g = col.getGreen(), b = col.getBlue(), ba = col.getAlpha();
+	int w = alpha.getWidth(), h = alpha.getHeight();
+	for(int y = 0; y < h; y++) {
+	    for(int x = 0; x < w; x++) {
+		int a = (alpha.getSample(x, y, 0) * ba) / 255;
+		int dx = x + ul.x, dy = y + ul.y;
+		dst.setSample(dx, dy, 0, ((r * a) + (dst.getSample(dx, dy, 0) * (255 - a))) / 255);
+		dst.setSample(dx, dy, 1, ((g * a) + (dst.getSample(dx, dy, 1) * (255 - a))) / 255);
+		dst.setSample(dx, dy, 2, ((b * a) + (dst.getSample(dx, dy, 2) * (255 - a))) / 255);
+		dst.setSample(dx, dy, 3, Math.max((ba * a) / 255, dst.getSample(dx, dy, 3)));
+	    }
+	}
+    }
+
+    public static BufferedImage blurmask(BufferedImage img, int rad, Color col) {
+	Coord bsz = imgsz(img), sz = bsz.add(rad * 4, rad * 4);
+	WritableRaster mask = alpharaster(sz);
+	mask.setSamples(rad * 2, rad * 2, bsz.x, bsz.y, 0, img.getRaster().getSamples(0, 0, bsz.x, bsz.y, 3, (int[])null));
+	imggrow(mask, rad);
+	imgblur(mask, rad, rad);
+	BufferedImage out = TexI.mkbuf(sz);
+	alphadraw(out.getRaster(), mask, Coord.z, col);
+	return(out);
+    }
+
+    public static BufferedImage blurmask2(BufferedImage img, int rad, Color col) {
+	BufferedImage mask = blurmask(img, rad, col);
+	Graphics g = mask.getGraphics();
+	g.drawImage(img, rad * 2, rad * 2, null);
+	g.dispose();
+	return(mask);
+    }
+
+    public static void dumpband(Raster img, int band) {
+	int w = img.getWidth(), h = img.getHeight();
+	for(int y = 0; y < h; y++) {
+	    for(int x = 0; x < w; x++) {
+		System.err.print((char)('a' + ((img.getSample(x, y, band) * ('z' - 'a')) / 255)));
+	    }
+	    System.err.println();
+	}
+    }
+
     public static int floordiv(int a, int b) {
 	if(a < 0)
 	    return(((a + 1) / b) - 1);
