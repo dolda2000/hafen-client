9c8dae896f166f829246c43cc14207f4d84a9afa
Fredrik Tolf <fredrik@dolda2000.com>
2013-10-09 03:56:01 +0200

    Fixed state applier consistency better.

--- src/haven/GLState.java
+++ src/haven/GLState.java
@@ -338,8 +338,8 @@ public abstract class GLState {
 	public final GL2 gl;
 	public final GLConfig cfg;
 	private boolean[] trans = new boolean[0], repl = new boolean[0], adirty = new boolean[0];
-	private ShaderMacro[][] shaders = new ShaderMacro[0][];
-	private int proghash = 0;
+	private ShaderMacro[][] shaders = new ShaderMacro[0][], nshaders = new ShaderMacro[0][];
+	private int proghash = 0, nproghash = 0;
 	public ShaderMacro.Program prog;
 	public boolean usedprog;
 	public boolean pdirty = false, sdirty = false;
@@ -398,25 +398,26 @@ public abstract class GLState {
 		    trans = new boolean[slotnum];
 		    repl = new boolean[slotnum];
 		    shaders = Utils.extend(shaders, slotnum);
+		    nshaders = Utils.extend(shaders, slotnum);
 		}
 	    }
 	    bufdiff(cur, next, trans, repl);
 	    Slot<?>[] deplist = GLState.deplist;
-	    boolean dirty = false;
+	    nproghash = proghash;
 	    for(int i = trans.length - 1; i >= 0; i--) {
-		if(sdirty || repl[i] || trans[i]) {
+		nshaders[i] = shaders[i];
+		if(repl[i] || trans[i]) {
 		    GLState nst = next.states[i];
 		    ShaderMacro[] ns = (nst == null)?null:nst.shaders();
-		    if(ns != shaders[i]) {
-			proghash ^= System.identityHashCode(shaders[i]) ^ System.identityHashCode(ns);
-			shaders[i] = ns;
-			dirty = true;
+		    if(ns != nshaders[i]) {
+			nproghash ^= System.identityHashCode(nshaders[i]) ^ System.identityHashCode(ns);
+			nshaders[i] = ns;
+			sdirty = true;
 		    }
 		}
 	    }
 	    usedprog = prog != null;
-	    if(dirty) {
-		sdirty = true;
+	    if(sdirty) {
 		ShaderMacro.Program np;
 		boolean usesl;
 		switch(g.gc.pref.progmode.val) {
@@ -426,7 +427,7 @@ public abstract class GLState {
 		case REQ:
 		    usesl = false;
 		    for(int i = 0; i < trans.length; i++) {
-			if((shaders[i] != null) && next.states[i].reqshaders()) {
+			if((nshaders[i] != null) && next.states[i].reqshaders()) {
 			    usesl = true;
 			    break;
 			}
@@ -438,7 +439,7 @@ public abstract class GLState {
 		    break;
 		}
 		if(usesl) {
-		    np = findprog(proghash, shaders);
+		    np = findprog(nproghash, nshaders);
 		} else {
 		    np = null;
 		}
@@ -469,6 +470,8 @@ public abstract class GLState {
 			    stcheckerr(g, "unapply", cur.states[id]);
 		    }
 		    cur.states[id] = null;
+		    proghash ^= System.identityHashCode(shaders[id]);
+		    shaders[id] = null;
 		}
 	    }
 	    /* Note on invariants: States may exit non-locally
@@ -483,6 +486,8 @@ public abstract class GLState {
 		    if(next.states[id] != null) {
 			next.states[id].apply(g);
 			cur.states[id] = next.states[id];
+			proghash ^= System.identityHashCode(shaders[id]) ^ System.identityHashCode(nshaders[id]);
+			shaders[id] = nshaders[id];
 			if(debug)
 			    stcheckerr(g, "apply", cur.states[id]);
 		    }
@@ -494,6 +499,8 @@ public abstract class GLState {
 			stcheckerr(g, "applyto", cur.states[id]);
 		    next.states[id].applyfrom(g, cur.states[id]);
 		    cur.states[id] = next.states[id];
+		    proghash ^= System.identityHashCode(shaders[id]) ^ System.identityHashCode(nshaders[id]);
+		    shaders[id] = nshaders[id];
 		    if(debug)
 			stcheckerr(g, "applyfrom", cur.states[id]);
 		    if(!pdirty && (prog != null))
