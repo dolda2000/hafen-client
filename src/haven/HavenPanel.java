c1a7d913f073ee9d4026421d7f7db18b1232989e
Fredrik Tolf <fredrik@dolda2000.com>
2013-10-11 11:01:04 +0200

    Save previous debugflags for transitions.

--- src/haven/HavenPanel.java
+++ src/haven/HavenPanel.java
@@ -421,6 +421,7 @@ public class HavenPanel extends GLCanvas implements Runnable, Console.Directory 
 	    int frames = 0, waited = 0;
 	    fthen = System.currentTimeMillis();
 	    while(true) {
+		Debug.cycle();
 		UI ui = this.ui;
 		then = System.currentTimeMillis();
 		if(Config.profile)
