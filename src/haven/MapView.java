0ae2e38fd0856b964498295cc012162c9dfac5ce
Fredrik Tolf <fredrik@dolda2000.com>
2013-10-22 04:56:05 +0200

    Added support for server-defined weather effects.

--- src/haven/MapView.java
+++ src/haven/MapView.java
@@ -490,7 +490,7 @@ public class MapView extends PView implements DTarget, Console.Directory {
 	}
     }
 
-    public Light amb = null;
+    public DirLight amb = null;
     private Outlines outlines = new Outlines(false);
     public void setup(RenderList rl) {
 	Gob pl = player();
@@ -504,6 +504,12 @@ public class MapView extends PView implements DTarget, Console.Directory {
 		amb = light;
 	    } else {
 		amb = null;
+	    }
+	    for(Glob.Weather w : glob.weather)
+		w.gsetup(rl);
+	    for(Glob.Weather w : glob.weather) {
+		if(w instanceof Rendered)
+		    rl.add((Rendered)w, null);
 	    }
 	}
 	if(rl.cfg.pref.outline.val)
