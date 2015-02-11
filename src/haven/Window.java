61ed7677a834a1940092a27c6113d48e819920af
Fredrik Tolf <fredrik@dolda2000.com>
2015-02-11 21:50:44 +0100

    Increase large-window margin somewhat.

--- src/haven/Window.java
+++ src/haven/Window.java
@@ -48,7 +48,7 @@ public class Window extends Widget implements DTarget {
     public static final Tex br = Resource.loadtex("gfx/hud/wnd/lg/br");
     public static final Coord tlm = new Coord(18, 30), brm = new Coord(13, 22), cpo = new Coord(36, 17);
     public static final int capo = 7, capio = 2;
-    public static final Coord dlmrgn = new Coord(23, 9), dsmrgn = new Coord(9, 9);
+    public static final Coord dlmrgn = new Coord(23, 14), dsmrgn = new Coord(9, 9);
     public static final BufferedImage ctex = Resource.loadimg("gfx/hud/fonttex");
     public static final Text.Furnace cf = new Text.Imager(new PUtils.TexFurn(new Text.Foundry(Text.fraktur, 15).aa(true), ctex)) {
 	    protected BufferedImage proc(Text text) {
