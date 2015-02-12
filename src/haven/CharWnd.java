f226ca0c08c376bee5ecb79e37cff8adc70d20b5
Fredrik Tolf <fredrik@dolda2000.com>
2015-02-12 01:04:46 +0100

    Added sattr display to charwnd.

--- src/haven/CharWnd.java
+++ src/haven/CharWnd.java
@@ -39,13 +39,15 @@ public class CharWnd extends Window {
     public static final Text.Foundry attrf = new Text.Foundry(Text.fraktur, 18).aa(true);
     public static final Color debuff = new Color(255, 128, 128);
     public static final Color buff = new Color(128, 255, 128);
+    public static final Color tbuff = new Color(128, 128, 255);
     public static final Color every = new Color(255, 255, 255, 16), other = new Color(255, 255, 255, 32);
     public final Collection<Attr> base;
+    public final Collection<SAttr> skill;
     public final FoodMeter feps;
     public final Constipations cons;
     public int exp;
+    private int scost;
     private final Tabs.Tab sattr;
-    private final Label explbl;
 
     public static class FoodMeter extends Widget {
 	public static final Tex frame = Resource.loadtex("gfx/hud/chr/foodm");
@@ -288,7 +290,7 @@ public class CharWnd extends Window {
 	public final Color bg;
 	private double lvlt = 0.0;
 	private Text ct;
-	private int cv;
+	private int cbv, ccv;
 
 	private Attr(String attr, String rnm, Coord c, Widget parent, Color bg) {
 	    super(c, new Coord(attrw, attrf.height() + 2), parent);
@@ -300,19 +302,19 @@ public class CharWnd extends Window {
 	}
 
 	public void tick(double dt) {
-	    if(attr.comp != cv) {
-		cv = attr.comp;
+	    if((attr.base != cbv) || (attr.comp != ccv)) {
+		cbv = attr.base; ccv = attr.comp;
 		Color c = Color.WHITE;
-		if(attr.comp > attr.base) {
+		if(ccv > cbv) {
 		    c = buff;
-		    tooltip = Text.render(String.format("%d + %d", attr.base, attr.comp - attr.base));
-		} else if(attr.comp < attr.base) {
+		    tooltip = Text.render(String.format("%d + %d", cbv, ccv - cbv));
+		} else if(ccv < cbv) {
 		    c = debuff;
-		    tooltip = Text.render(String.format("%d - %d", attr.base, attr.base - attr.comp));
+		    tooltip = Text.render(String.format("%d - %d", cbv, cbv - ccv));
 		} else {
 		    tooltip = null;
 		}
-		ct = attrf.render(Integer.toString(cv), c);
+		ct = attrf.render(Integer.toString(ccv), c);
 	    }
 	    if((lvlt > 0.0) && ((lvlt -= dt) < 0))
 		lvlt = 0.0;
@@ -336,8 +338,154 @@ public class CharWnd extends Window {
 	}
     }
 
-    private void updexp() {
-	explbl.settext(Utils.thformat(exp));
+    public class SAttr extends Widget {
+	public final String nm;
+	public final Text rnm;
+	public final Glob.CAttr attr;
+	public final Tex img;
+	public final Color bg;
+	public int tbv, tcv, cost;
+	private Text ct;
+	private int cbv, ccv;
+
+	private SAttr(String attr, String rnm, Coord c, Widget parent, Color bg) {
+	    super(c, new Coord(attrw, attrf.height() + 2), parent);
+	    this.nm = attr;
+	    this.img = Resource.load("gfx/hud/chr/" + attr).loadwait().layer(Resource.imgc).tex();
+	    this.rnm = attrf.render(rnm);
+	    this.attr = ui.sess.glob.cattr.get(attr);
+	    this.bg = bg;
+	    new IButton(new Coord(sz.x - 19, (sz.y / 2) - 7), this, "gfx/hud/buttons/add", "u", "d", null) {
+		public void click() {adj(1);}
+	    };
+	    new IButton(new Coord(sz.x - 34, (sz.y / 2) - 7), this, "gfx/hud/buttons/sub", "u", "d", null) {
+		public void click() {adj(-1);}
+		public boolean mousewheel(Coord c, int a) {adj(-a); return(true);}
+	    };
+	}
+
+	public void tick(double dt) {
+	    if((attr.base != cbv) || (attr.comp != ccv)) {
+		cbv = attr.base; ccv = attr.comp;
+		if(tbv <= cbv) {
+		    tbv = cbv; tcv = ccv;
+		    updcost();
+		}
+		Color c = Color.WHITE;
+		if(ccv > cbv) {
+		    c = buff;
+		    tooltip = Text.render(String.format("%d + %d", cbv, ccv - cbv));
+		} else if(ccv < cbv) {
+		    c = debuff;
+		    tooltip = Text.render(String.format("%d - %d", cbv, cbv - ccv));
+		} else {
+		    tooltip = null;
+		}
+		if(tcv > ccv)
+		    c = tbuff;
+		ct = attrf.render(Integer.toString(tcv), c);
+	    }
+	}
+
+	public void draw(GOut g) {
+	    g.chcolor(bg);
+	    g.frect(Coord.z, sz);
+	    g.chcolor();
+	    super.draw(g);
+	    Coord cn = new Coord(0, sz.y / 2);
+	    g.aimage(img, cn.add(5, 0), 0, 0.5);
+	    g.aimage(rnm.tex(), cn.add(img.sz().x + 10, 1), 0, 0.5);
+	    g.aimage(ct.tex(), cn.add(sz.x - 40, 1), 1, 0.5);
+	}
+
+	private void updcost() {
+	    int cost = 100 * ((tbv + (tbv * tbv)) - (attr.base + (attr.base * attr.base))) / 2;
+	    scost += cost - this.cost;
+	    this.cost = cost;
+	}
+
+	public void adj(int a) {
+	    if(tbv + a < attr.base) a = attr.base - tbv;
+	    tbv += a; tcv += a;
+	    cbv = ccv = 0;
+	    updcost();
+	}
+
+	public boolean mousewheel(Coord c, int a) {
+	    adj(-a);
+	    return(true);
+	}
+    }
+
+    public static class RLabel extends Label {
+	private Coord oc;
+
+	public RLabel(Coord c, Widget parent, String text) {
+	    super(c, parent, text);
+	    oc = c;
+	    this.c = oc.add(-sz.x, 0);
+	}
+
+	public void settext(String text) {
+	    super.settext(text);
+	    this.c = oc.add(-sz.x, 0);
+	}
+    }
+
+    public class ExpLabel extends RLabel {
+	private int cexp;
+
+	public ExpLabel(Coord c, Widget parent) {
+	    super(c, parent, "0");
+	}
+
+	public void draw(GOut g) {
+	    super.draw(g);
+	    if(exp != cexp)
+		settext(Utils.thformat(cexp = exp));
+	}
+    }
+
+    public class StudyInfo extends Widget {
+	public Widget study;
+	public int texp, tw;
+	private final Text.UText<?> texpt = new Text.UText<Integer>(Text.std) {
+	    public Integer value() {return(texp);}
+	    public String text(Integer v) {return(Utils.thformat(v));}
+	};
+	private final Text.UText<?> twt = new Text.UText<Integer>(Text.std) {
+	    private Glob.CAttr intv = ui.sess.glob.cattr.get("int");
+	    public Integer value() {return(tw);}
+	    public String text(Integer v) {return(v + "/" + intv.comp);}
+	};
+
+	private StudyInfo(Coord c, Coord sz, Widget parent, Widget study) {
+	    super(c, sz, parent);
+	    this.study = study;
+	    new Label(new Coord(2, 2), this, "Attention:");
+	    new Label(new Coord(2, sz.y - 32), this, "Learning points:");
+	}
+
+	private void upd() {
+	    int texp = 0, tw = 0;
+	    for(GItem item : study.children(GItem.class)) {
+		Curiosity ci = ItemInfo.find(Curiosity.class, item.info());
+		if(ci != null) {
+		    texp += ci.exp;
+		    tw += ci.mw;
+		}
+	    }
+	    this.texp = texp; this.tw = tw;
+	}
+
+	public void draw(GOut g) {
+	    upd();
+	    super.draw(g);
+	    g.chcolor(255, 192, 255, 255);
+	    g.aimage(twt.get().tex(), new Coord(sz.x - 2, 17), 1.0, 0.0);
+	    g.chcolor(192, 192, 255, 255);
+	    g.aimage(texpt.get().tex(), sz.add(-2, -15), 1.0, 0.0);
+	}
     }
 
     @RName("chr")
@@ -379,9 +527,46 @@ public class CharWnd extends Window {
 	}
 
 	{
+	    int x = 5, y = 0;
+
 	    sattr = tabs.new Tab();
-	    new Label(new Coord(0, 150), sattr, "Learning Points:");
-	    explbl = new Label(new Coord(90, 150), sattr, "0");
+	    new Img(new Coord(x - 5, y), catf.render("Skill Values").tex(), sattr); y += 35;
+	    skill = new ArrayList<SAttr>();
+	    SAttr aw;
+	    skill.add(aw = new SAttr("unarmed", "Unarmed Combat", wbox.btloff().add(x, y), sattr, every)); y += aw.sz.y;
+	    Frame.around(sattr, skill);
+
+	    x = 260; y = 0;
+	    new Img(new Coord(x - 5, y), catf.render("Study Report").tex(), sattr); y += 35;
+	    y += 150;
+	    int rx = x + attrw - 10;
+	    new Frame(new Coord(x, y).add(wbox.btloff()), new Coord(attrw, 75), sattr);
+	    new Label(new Coord(x + 15, y + 10), sattr, "Learning points:");
+	    new ExpLabel(new Coord(rx, y + 10), sattr);
+	    new Label(new Coord(x + 15, y + 25), sattr, "Cost:");
+	    new RLabel(new Coord(rx, y + 25), sattr, "0") {
+		int cc;
+
+		public void draw(GOut g) {
+		    if(cc > exp)
+			g.chcolor(debuff);
+		    super.draw(g);
+		    if(cc != scost)
+			settext(Utils.thformat(cc = scost));
+		}
+	    };
+	    new Button(new Coord(rx - 75, y + 50), 75, sattr, "Buy") {
+		public void click() {
+		    ArrayList<Object> args = new ArrayList<Object>();
+		    for(SAttr attr : skill) {
+			if(attr.tbv > attr.attr.base) {
+			    args.add(attr.attr.nm);
+			    args.add(attr.tbv);
+			}
+		    }
+		    CharWnd.this.wdgmsg("sattr", args.toArray(new Object[0]));
+		}
+	    };
 	}
 
 	{
@@ -410,7 +595,11 @@ public class CharWnd extends Window {
     public Widget makechild(String type, Object[] pargs, Object[] cargs) {
 	String place = (pargs[0] instanceof String)?(((String)pargs[0]).intern()):null;
 	if(place == "study") {
-	    return(gettype(type).create(Coord.z, sattr, cargs));
+	    Widget ret = gettype(type).create(new Coord(260, 35).add(wbox.btloff()), sattr, cargs);
+	    Frame.around(sattr, Collections.singletonList(ret));
+	    Widget inf = new StudyInfo(new Coord(260 + 150, ret.c.y).add(wbox.btloff().x, 0), new Coord(attrw - 150, ret.sz.y), sattr, ret);
+	    Frame.around(sattr, Collections.singletonList(inf));
+	    return(ret);
 	} else {
 	    return(super.makechild(type, pargs, cargs));
 	}
@@ -419,7 +608,6 @@ public class CharWnd extends Window {
     public void uimsg(String nm, Object... args) {
 	if(nm == "exp") {
 	    exp = ((Number)args[0]).intValue();
-	    updexp();
 	} else if(nm == "food") {
 	    feps.update(args);
 	} else if(nm == "ftrig") {
