c1a95550adca1e522d8aca9754b932a99fcb9e1e
Marat Vildanov <marat.vildanov@gmail.com>
2015-09-14 20:31:05 +0300

    Don't save visibility setting for panels which user can't hide manually

--- src/haven/GameUI.java
+++ src/haven/GameUI.java
@@ -112,10 +112,10 @@ public class GameUI extends ConsoleHost implements Console.Directory {
 	    chat.show();
 	}
 	beltwdg.raise();
-	ulpanel = add(new Hidepanel("gui-ul", null, new Coord(-1, -1)));
-	urpanel = add(new Hidepanel("gui-ur", null, new Coord( 1, -1)));
-	blpanel = add(new Hidepanel("gui-bl", null, new Coord(-1,  1)));
-	brpanel = add(new Hidepanel("gui-br", null, new Coord( 1,  1)) {
+	ulpanel = add(new Hidepanel("gui-ul", null, new Coord(-1, -1), false));
+	urpanel = add(new Hidepanel("gui-ur", null, new Coord( 1, -1), true));
+	blpanel = add(new Hidepanel("gui-bl", null, new Coord(-1,  1), true));
+	brpanel = add(new Hidepanel("gui-br", null, new Coord( 1,  1), true) {
 		public void move(double a) {
 		    super.move(a);
 		    menupanel.move();
@@ -125,7 +125,7 @@ public class GameUI extends ConsoleHost implements Console.Directory {
 		    public Coord get() {
 			return(new Coord(GameUI.this.sz.x, Math.min(brpanel.c.y - 79, GameUI.this.sz.y - menupanel.sz.y)));
 		    }
-		}, new Coord(1, 0)));
+		}, new Coord(1, 0), true));
 	blpanel.add(new Img(Resource.loadtex("gfx/hud/blframe")), 0, 9);
 	blpanel.add(new Img(Resource.loadtex("gfx/hud/lbtn-bg")), 0, 0);
 	menu = brpanel.add(new MenuGrid(), 20, 34);
@@ -282,12 +282,15 @@ public class GameUI extends ConsoleHost implements Console.Directory {
 	public final Indir<Coord> base;
 	public boolean tvis;
 	private double cur;
+	private boolean persist;
 
-	public Hidepanel(String id, Indir<Coord> base, Coord g) {
+	public Hidepanel(String id, Indir<Coord> base, Coord g, boolean persist) {
 	    this.id = id;
 	    this.base = base;
 	    this.g = g;
-	    cur = show(tvis = Utils.getprefb(id + "-visible", true))?0:1;
+	    this.persist = persist;
+	    this.tvis = persist ? Utils.getprefb(id + "-visible", true) : true;
+	    cur = show(tvis)?0:1;
 	}
 
 	public <T extends Widget> T add(T child) {
@@ -345,11 +348,12 @@ public class GameUI extends ConsoleHost implements Console.Directory {
 	}
 
 	public boolean mshow() {
-	    return(mshow(Utils.getprefb(id + "-visible", true)));
+	    return(mshow(persist ? Utils.getprefb(id + "-visible", true) : true));
 	}
 
 	public boolean cshow(boolean vis) {
-	    Utils.setprefb(id + "-visible", vis);
+	    if (persist)
+	        Utils.setprefb(id + "-visible", vis);
 	    if(vis != tvis)
 		mshow(vis);
 	    return(vis);
