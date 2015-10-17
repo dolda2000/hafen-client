f5ad81fad079f9075562e8c0318301503b40ecc5
Marat Vildanov <marat.vildanov@gmail.com>
2015-10-17 11:25:16 +0300

    Implement grabber that allows to select a single tile

---
+++ src/haven/GrabberWnd.java
@@ -0,0 +1,64 @@
+package haven;
+
+import java.awt.event.KeyEvent;
+
+public abstract class GrabberWnd extends Window implements MapView.Grabber {
+    private final GameUI gui;
+    private final Widget btn;
+    private final MapView.Grabber grabber;
+    private final Label label;
+
+    public GrabberWnd(String title, String label, GameUI gui) {
+        super(new Coord(150, 50), title);
+        this.label = add(new Label(label));
+        this.btn = new Button(40, "Done");
+        add(btn, this.asz.sub(btn.sz));
+        this.grabber = gui.map.new GrabXL(this);
+        this.gui = gui;
+        this.gui.map.enol(16);
+        this.gui.map.grab(grabber);
+    }
+
+    protected final void setLabel(String text) {
+        label.settext(text);
+    }
+
+    @Override
+    public void show() {
+        super.show();
+        parent.setfocus(this);
+    }
+
+    public final void close() {
+        this.cbtn.click();
+    }
+
+    protected abstract void done();
+
+    @Override
+    public void destroy() {
+        gui.map.disol(16);
+        gui.map.release(grabber);
+        super.destroy();
+    }
+
+    public boolean type(char key, java.awt.event.KeyEvent ev) {
+        if(key == KeyEvent.VK_ENTER) {
+            done();
+            close();
+        }
+        return true;
+    }
+
+    @Override
+    public void wdgmsg(Widget sender, String msg, Object... args) {
+        if((sender == this) && (msg.equals("close"))) {
+            destroy();
+        } else if (sender == this.btn) {
+            done();
+            close();
+        } else {
+            super.wdgmsg(sender, msg, args);
+        }
+    }
+}
---
+++ src/haven/TileGrabber.java
@@ -0,0 +1,62 @@
+package haven;
+
+public class TileGrabber extends GrabberWnd {
+    static final String title = "Tile selection";
+    static final String defaultText = "Selected tile: (none)";
+    Coord c;
+    MCache.Overlay ol;
+    final MCache map;
+
+    public TileGrabber(String title, GameUI gui) {
+        super(title, defaultText, gui);
+        this.map = gui.ui.sess.glob.map;
+    }
+
+    @Override
+    public void destroy() {
+        if (this.ol != null)
+            this.ol.destroy();
+        super.destroy();
+    }
+
+    @Override
+     protected void done() {
+        done(c);
+    }
+
+    protected void done(Coord tile) {
+    }
+
+    @Override
+    public boolean mmousedown(Coord mc, int button) {
+        c = mc.div(MCache.tilesz);
+        if (this.ol != null)
+            this.ol.destroy();
+        this.ol = map.new Overlay(c, c, 1 << 16);
+        setLabel(String.format("Select tile: (%d, %d)", c.x, c.y));
+        return true;
+    }
+
+    @Override
+    public boolean mmouseup(Coord mc, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean mmousewheel(Coord mc, int amount) {
+        return false;
+    }
+
+    @Override
+    public void mmousemove(Coord mc) {
+    }
+
+    @Override
+    public void uimsg(String msg, Object... args) {
+        if (msg.equals("reset")) {
+            this.ol.destroy();
+            this.ol = null;
+            this.c = null;
+        }
+    }
+}
