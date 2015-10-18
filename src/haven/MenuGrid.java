b5cb4f5cf25222bc5a973d4cf90adf6430467dc9
Marat Vildanov <marat.vildanov@gmail.com>
2015-10-18 14:09:55 +0300

    Add tool to plant trees in the center of tile

--- src/haven/MenuGrid.java
+++ src/haven/MenuGrid.java
@@ -31,6 +31,8 @@ import java.awt.event.KeyEvent;
 import java.awt.font.TextAttribute;
 import haven.Resource.AButton;
 import haven.Glob.Pagina;
+import haven.util.ObservableCollection;
+
 import java.util.*;
 
 public class MenuGrid extends Widget {
@@ -111,6 +113,14 @@ public class MenuGrid extends Widget {
     public MenuGrid() {
 	super(bgsz.mul(gsz).add(1, 1));
     }
+
+	@Override
+	protected void attach(UI ui) {
+		super.attach(ui);
+		Glob glob = ui.sess.glob;
+		ObservableCollection<Pagina> p = glob.paginae;
+		p.add(glob.paginafor(Resource.local().load("paginae/custom/plant-tree")));
+	}
 	
     private static Comparator<Pagina> sorter = new Comparator<Pagina>() {
 	public int compare(Pagina a, Pagina b) {
@@ -298,12 +308,38 @@ public class MenuGrid extends Widget {
 		curoff += 14;
 	} else {
 	    r.newp = 0;
-	    wdgmsg("act", (Object[])r.act().ad);
+	    use(r);
 	    if(reset)
 		this.cur = null;
 	    curoff = 0;
 	}
 	updlayout();
+    }
+
+    public boolean use(Pagina r) {
+        String [] ad = r.act().ad;
+        if((ad == null) || (ad.length < 1)){
+            return false;
+        }
+        if(ad[0].equals("@")) {
+            usecustom(ad);
+        } else {
+            wdgmsg("act", (Object[])ad);
+        }
+        return true;
+    }
+
+    private void usecustom(String[] ad) {
+        if(ad[1].equals("plant-tree")) {
+            if (ui != null && ui.gui != null) {
+                ui.gui.add(new TileGrabber("Select tile...", ui.gui) {
+                    @Override
+                    protected void done(Coord tile) {
+                        ui.gui.map.wdgmsg("itemact", Coord.z, tile.mul(MCache.tilesz).add(MCache.tilesz.div(2)), ui.modflags());
+                    }
+                }, 300, 200);
+            }
+        }
     }
 
     public void tick(double dt) {
