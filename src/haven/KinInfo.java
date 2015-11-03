e80317ee3931b1cb70e654ea2a5dc1e668c84c58
Marat Vildanov <marat.vildanov@gmail.com>
2015-11-03 04:48:54 +0300

    Always show kin info only for players

--- src/haven/KinInfo.java
+++ src/haven/KinInfo.java
@@ -101,7 +101,9 @@ public class KinInfo extends GAttrib {
 			int tm = (int)(now - seen);
 			Color show = null;
 			boolean auto = (type & 1) == 0;
-			if(auto && (tm < 7500)) {
+			if (gob.isPlayer()) {
+				show = Color.WHITE;
+			} else if(auto && (tm < 7500)) {
 			    show = Utils.clipcol(255, 255, 255, 255 - ((255 * tm) / 7500));
 			}
 			if(show != null) {
