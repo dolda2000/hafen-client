d8efb4b439883a598755eb5396e2398513e641dd
Marat Vildanov <marat.vildanov@gmail.com>
2015-11-07 23:14:09 +0300

    Do not update z-coordinate of radius sprite hopefully fixing random crashes

--- src/haven/GobRadius.java
+++ src/haven/GobRadius.java
@@ -54,8 +54,8 @@ public class GobRadius {
             this.color = color;
         }
 
-        public Sprite create(Sprite.Owner owner) {
-            return new RadiusSprite(owner, radius, color);
+        public Sprite create(Gob owner) {
+            return new RadiusSprite(owner, radius, owner.getc().z, color);
         }
     }
 
@@ -67,18 +67,18 @@ public class GobRadius {
         private Coord lc;
         float[] barda;
 
-        protected RadiusSprite(Sprite.Owner owner, float r, Color color) {
+        protected RadiusSprite(Sprite.Owner owner, float radius, float z, Color color) {
             super(owner, null);
             this.smat = new States.ColState(color);
-            int i = Math.max(24, (int)(Math.PI * 2 * r / 11.0D));
+            int i = Math.max(24, (int)(Math.PI * 2 * radius / 11.0D));
             FloatBuffer posa = Utils.mkfbuf(i * 3 * 2);
             FloatBuffer nrma = Utils.mkfbuf(i * 3 * 2);
             ShortBuffer sidx = Utils.mksbuf(i * 6);
             for (int j = 0; j < i; j++) {
                 float s = (float)Math.sin(Math.PI * 2 * j / i);
                 float c = (float)Math.cos(Math.PI * 2 * j / i);
-                posa.put(j * 3, c * r).put(j * 3 + 1, s * r).put(j * 3 + 2, 10.0F);
-                posa.put((i + j) * 3, c * r).put((i + j) * 3 + 1, s * r).put((i + j) * 3 + 2, -10.0F);
+                posa.put(j * 3, c * radius).put(j * 3 + 1, s * radius).put(j * 3 + 2, z + 20.0F);
+                posa.put((i + j) * 3, c * radius).put((i + j) * 3 + 1, s * radius).put((i + j) * 3 + 2, z);
                 nrma.put(j * 3, c).put(j * 3 + 1, s).put(j * 3 + 2, 0.0F);
                 nrma.put((i + j) * 3, c).put((i + j) * 3 + 1, s).put((i + j) * 3 + 2, 0.0F);
                 int k = j * 6;
@@ -98,7 +98,7 @@ public class GobRadius {
             this.posa.bind(g, false);
             this.nrma.bind(g, false);
             this.sidx.rewind();
-            g.gl.glDrawElements(GL.GL_TRIANGLES, this.sidx.capacity(), GL.GL_UNSIGNED_SHORT, this.sidx);
+            g.gl.glDrawElements(GL.GL_TRIANGLES, this.sidx.limit(), GL.GL_UNSIGNED_SHORT, this.sidx);
 
             this.posa.unbind(g);
             this.nrma.unbind(g);
@@ -111,34 +111,6 @@ public class GobRadius {
             Location.goback(d.state(), "gobx");
             d.state().put(States.color, null);
             return true;
-        }
-
-        @Override
-        public boolean tick(int dt) {
-            Coord c = ((Gob)this.owner).rc;
-            if ((this.lc == null) || (!this.lc.equals(c)))
-            {
-                setz(this.owner.glob(), c);
-                this.lc = c;
-            }
-            return false;
-        }
-
-        private void setz(Glob glob, Coord c)
-        {
-            FloatBuffer posa = this.posa.data;
-            int i = this.posa.size() / 2;
-            try
-            {
-                float f1 = glob.map.getcz(c.x, c.y);
-                for (int j = 0; j < i; j++)
-                {
-                    float f2 = glob.map.getcz(c.x + posa.get(j * 3), c.y - posa.get(j * 3 + 1)) - f1;
-                    posa.put(j * 3 + 2, f2 + 10.0F);
-                    posa.put((i + j) * 3 + 2, f2);
-                }
-            }
-            catch (Loading localLoading) {}
         }
     }
 }
