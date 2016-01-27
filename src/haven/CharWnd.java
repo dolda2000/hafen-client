dae79e9a08188557d4353f9c4e6f66fa7c1d3432
Fredrik Tolf <fredrik@dolda2000.com>
2016-01-27 22:02:19 +0100

    Revert "Display total food satiation as a color overlay."
    This reverts commit 19a92ccb36ac2158c7d2765a3cf200467c3d098d.

--- src/haven/CharWnd.java
+++ src/haven/CharWnd.java
@@ -1784,7 +1784,6 @@ public class CharWnd extends Window {
 		double m = ((Number)args[a++]).doubleValue();
 		cons.update(t, m);
 	    }
-	    WItem.cacheseq++;
 	} else if(nm == "csk") {
 	    /* One *could* argue that rmessages should have some
 	     * built-in fragmentation scheme. ^^ */
