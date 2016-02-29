e0f637a14f886381dba0ee656161ee673dbbfdaa
Fredrik Tolf <fredrik@dolda2000.com>
2016-02-29 21:05:08 +0100

    Use mapview cc for minimap when player is not present.

--- src/haven/LocalMiniMap.java
+++ src/haven/LocalMiniMap.java
@@ -176,11 +176,10 @@ public class LocalMiniMap extends Widget {
 
     public void tick(double dt) {
 	Gob pl = ui.sess.glob.oc.getgob(mv.plgob);
-	if(pl == null) {
-	    this.cc = null;
-	    return;
-	}
-	this.cc = pl.rc.div(tilesz);
+	if(pl == null)
+	    this.cc = mv.cc.div(tilesz);
+	else
+	    this.cc = pl.rc.div(tilesz);
     }
 
     public void draw(GOut g) {
