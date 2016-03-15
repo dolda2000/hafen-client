4d1da6191b8b5146e6c214ef3d1ab7bfa5b14299
Fredrik Tolf <fredrik@dolda2000.com>
2016-03-15 21:29:54 +0100

    Fixed lvlup message.

--- src/haven/CharWnd.java
+++ src/haven/CharWnd.java
@@ -165,7 +165,7 @@ public class CharWnd extends Window {
 		    Collections.sort(etr, dcmp);
 		    GameUI gui = getparent(GameUI.class);
 		    if(gui != null)
-			gui.msg(String.format("You gained " + Loading.waitfor(trev).layer(Event.class).nm));
+			gui.msg(String.format("You gained " + Loading.waitfor(trev).layer(Event.class).nm), Color.WHITE);
 		    trol = new TexI(mktrol(etr, trev));
 		    trtm = System.currentTimeMillis();
 		    trev = null;
