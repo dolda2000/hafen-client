c22deb021dd4156179fcf012fed0ce64c92cb67e
Fredrik Tolf <fredrik@dolda2000.com>
2017-01-10 22:19:20 +0100

    Added some debugging code for a chat-UI bug.

--- src/haven/ChatUI.java
+++ src/haven/ChatUI.java
@@ -462,7 +462,11 @@ public class ChatUI extends Widget {
 
 	protected void clicked(CharPos pos) {
 	    AttributedCharacterIterator inf = pos.part.ti();
-	    inf.setIndex(pos.ch.getCharIndex());
+	    try {
+		inf.setIndex(pos.ch.getCharIndex());
+	    } catch(IllegalArgumentException e) {
+		throw(new IllegalArgumentException(String.format("%d %d %d", pos.ch.getCharIndex(), pos.part.start, pos.part.end), e));
+	    }
 	    FuckMeGentlyWithAChainsaw url = (FuckMeGentlyWithAChainsaw)inf.getAttribute(ChatAttribute.HYPERLINK);
 	    if((url != null) && (WebBrowser.self != null)) {
 		try {
