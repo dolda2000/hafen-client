4765cd7b022f09c889c93db1b77982aa69f88411
Fredrik Tolf <fredrik@dolda2000.com>
2017-01-31 21:13:41 +0100

    Fixed a cache locking problem.

--- src/haven/HashDirCache.java
+++ src/haven/HashDirCache.java
@@ -117,15 +117,31 @@ public class HashDirCache implements ResCache {
 	fp.writeUTF(name);
     }
 
+    private static class LockedFile {
+	final RandomAccessFile f;
+	final FileLock l;
+	LockedFile(RandomAccessFile f, FileLock l) {this.f = f; this.l = l;}
+    }
+
     /* These locks should never have to be waited for long at all, so
      * blocking interruptions until complete should be perfectly
      * okay. */
-    private static FileLock lock2(FileChannel ch) throws IOException {
+    private static LockedFile lock2(File path) throws IOException {
 	boolean intr = false;
 	try {
 	    while(true) {
 		try {
-		    return(ch.lock());
+		    RandomAccessFile fp = null;
+		    try {
+			fp = new RandomAccessFile(path, "rw");
+			FileLock lk = fp.getChannel().lock();
+			LockedFile ret = new LockedFile(fp, lk);
+			fp = null;
+			return(ret);
+		    } finally {
+			if(fp != null)
+			    fp.close();
+		    }
 		} catch(FileLockInterruptionException e) {
 		    Thread.currentThread().interrupted();
 		    intr = true;
@@ -151,15 +167,13 @@ public class HashDirCache implements ResCache {
 		monitors.put(lfn, mon = new Object());
 	}
 	synchronized(mon) {
-	    RandomAccessFile lf = new RandomAccessFile(lfn, "rw");
-	    FileLock lk = null;
+	    LockedFile lf = lock2(lfn);
 	    try {
-		lk = lock2(lf.getChannel());
 		for(int idx = 0; ; idx++) {
 		    File path = new File(base, String.format("%016x.%d", h, idx));
 		    if(!path.exists() && !creat)
 			return(null);
-		    RandomAccessFile fp = (idx == 0)?lf:new RandomAccessFile(path, "rw");
+		    RandomAccessFile fp = (idx == 0)?lf.f:new RandomAccessFile(path, "rw");
 		    try {
 			Header head = readhead(fp);
 			if(head == null) {
@@ -177,9 +191,8 @@ public class HashDirCache implements ResCache {
 		    }
 		}
 	    } finally {
-		if(lk != null)
-		    lk.release();
-		lf.close();
+		lf.l.release();
+		lf.f.close();
 	    }
 	}
     }
