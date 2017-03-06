7694baeaa077f8d6490a70e85eaf642574da12c5
Fredrik Tolf <fredrik@dolda2000.com>
2017-03-06 23:11:11 +0100

    Change click handling on constipation list.

--- src/haven/CharWnd.java
+++ src/haven/CharWnd.java
@@ -404,8 +404,7 @@ public class CharWnd extends Window {
 	    order();
 	}
 
-	public boolean mousedown(Coord c, int button) {
-	    return(false);
+	protected void itemclick(El item, int button) {
 	}
     }
 
