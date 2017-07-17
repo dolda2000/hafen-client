46789110f9e8728f90a008cac6b3c1e498c07b59
Fredrik Tolf <fredrik@dolda2000.com>
2017-07-17 23:55:56 +0200

    Improved some char-window uimsg decoding.

--- src/haven/CharWnd.java
+++ src/haven/CharWnd.java
@@ -2106,30 +2106,36 @@ public class CharWnd extends Window {
 	}
     }
 
-    private void decsklist(Collection<Skill> buf, Object[] args, int a, boolean has) {
+    private List<Skill> decsklist(Object[] args, int a, boolean has) {
+	List<Skill> buf = new ArrayList<>();
 	while(a < args.length) {
 	    String nm = (String)args[a++];
 	    Indir<Resource> res = ui.sess.getres((Integer)args[a++]);
 	    int cost = ((Number)args[a++]).intValue();
 	    buf.add(new Skill(nm, res, cost, has));
 	}
+	return(buf);
     }
 
-    private void deccrlist(Collection<Credo> buf, Object[] args, int a, boolean has) {
+    private List<Credo> deccrlist(Object[] args, int a, boolean has) {
+	List<Credo> buf = new ArrayList<>();
 	while(a < args.length) {
 	    String nm = (String)args[a++];
 	    Indir<Resource> res = ui.sess.getres((Integer)args[a++]);
 	    buf.add(new Credo(nm, res, has));
 	}
+	return(buf);
     }
 
-    private void decexplist(Collection<Experience> buf, Object[] args, int a) {
+    private List<Experience> decexplist(Object[] args, int a) {
+	List<Experience> buf = new ArrayList<>();
 	while(a < args.length) {
 	    Indir<Resource> res = ui.sess.getres((Integer)args[a++]);
 	    int mtime = ((Number)args[a++]).intValue();
 	    int score = ((Number)args[a++]).intValue();
 	    buf.add(new Experience(res, mtime, score));
 	}
+	return(buf);
     }
 
     public void uimsg(String nm, Object... args) {
@@ -2157,25 +2163,19 @@ public class CharWnd extends Window {
 		cons.update(t, m);
 	    }
 	} else if(nm == "csk") {
-	    List<Skill> buf = new ArrayList<Skill>();
-	    decsklist(buf, args, 0, true);
-	    skg.csk.update(buf);
+	    skg.csk.update(decsklist(args, 0, true));
 	} else if(nm == "nsk") {
-	    List<Skill> buf = new ArrayList<Skill>();
-	    decsklist(buf, args, 0, false);
-	    skg.nsk.update(buf);
+	    skg.nsk.update(decsklist(args, 0, false));
 	} else if(nm == "ccr") {
-	    List<Credo> buf = new ArrayList<Credo>();
-	    deccrlist(buf, args, 0, true);
-	    credos.ccr(buf);
+	    credos.ccr(deccrlist(args, 0, true));
 	} else if(nm == "ncr") {
-	    List<Credo> buf = new ArrayList<Credo>();
-	    deccrlist(buf, args, 0, false);
-	    credos.ncr(buf);
+	    credos.ncr(deccrlist(args, 0, false));
+	} else if(nm == "pcr") {
+	    String cnm = (String)args[0];
+	    Indir<Resource> res = ui.sess.getres((Integer)args[1]);
+	    credos.pcr(new Credo(cnm, res, false));
 	} else if(nm == "exps") {
-	    List<Experience> buf = new ArrayList<Experience>();
-	    decexplist(buf, args, 0);
-	    exps.seen.update(buf);
+	    exps.seen.update(decexplist(args, 0));
 	} else if(nm == "wounds") {
 	    for(int i = 0; i < args.length; i += 3) {
 		int id = (Integer)args[i];
