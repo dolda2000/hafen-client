5c83f80a0e278be8410e21c65e7df11d9ac86a15
Fredrik Tolf <fredrik@dolda2000.com>
2017-09-14 17:05:23 +0200

    Bumped protocol version.

--- src/haven/Session.java
+++ src/haven/Session.java
@@ -32,7 +32,7 @@ import java.io.*;
 import java.lang.ref.*;
 
 public class Session {
-    public static final int PVER = 11;
+    public static final int PVER = 12;
 
     public static final int MSG_SESS = 0;
     public static final int MSG_REL = 1;
