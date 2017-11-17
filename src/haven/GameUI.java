c28a12caae22285f8f429b666183d04d952bbd80
Fredrik Tolf <fredrik@dolda2000.com>
2017-11-17 21:06:41 +0100

    Optionally include character ID in mapfile name.

--- src/haven/GameUI.java
+++ src/haven/GameUI.java
@@ -168,6 +168,17 @@ public class GameUI extends ConsoleHost implements Console.Directory {
 	}
     }
 
+    private String mapfilename() {
+	StringBuilder buf = new StringBuilder();
+	buf.append(genus);
+	String chrid = Utils.getpref("mapfile/" + this.chrid, "");
+	if(!chrid.equals("")) {
+	    if(buf.length() > 0) buf.append('/');
+	    buf.append(chrid);
+	}
+	return(buf.toString());
+    }
+
     public void addchild(Widget child, Object... args) {
 	String place = ((String)args[0]).intern();
 	if(place == "mapview") {
@@ -179,7 +190,7 @@ public class GameUI extends ConsoleHost implements Console.Directory {
 	    mmap = adda(new Frame(new Coord(125, 125), true), 0, sz.y, 0, 1);
 	    LocalMiniMap mm = mmap.add(new LocalMiniMap(new Coord(125, 125), map));
 	    if(ResCache.global != null)
-		mm.save(new MapFile(ResCache.global, genus));
+		mm.save(new MapFile(ResCache.global, mapfilename()));
 	} else if(place == "menu") {
 	    menu = (MenuGrid)add(child);
 	} else if(place == "fight") {
@@ -698,6 +709,11 @@ public class GameUI extends ConsoleHost implements Console.Directory {
 			Utils.setpref("belttype", "n");
 			resize(sz);
 		    }
+		}
+	    });
+	cmdmap.put("chrmap", new Console.Command() {
+		public void run(Console cons, String[] args) {
+		    Utils.setpref("mapfile/" + chrid, args[1]);
 		}
 	    });
 	cmdmap.put("tool", new Console.Command() {
