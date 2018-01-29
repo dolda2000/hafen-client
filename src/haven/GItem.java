3ac7f9e0fad84febff09555d90d671cb8ff7c65c
Fredrik Tolf <fredrik@dolda2000.com>
2018-01-30 00:48:55 +0100

    Generalized numberinfos to overlayinfos.

--- src/haven/GItem.java
+++ src/haven/GItem.java
@@ -53,11 +53,43 @@ public class GItem extends AWidget implements ItemInfo.SpriteOwner, GSprite.Owne
 	public Color olcol();
     }
 
-    public interface NumberInfo {
+    public interface OverlayInfo<T> {
+	public T overlay();
+	public void drawoverlay(GOut g, T data);
+    }
+
+    public static class InfoOverlay<T> {
+	public final OverlayInfo<T> inf;
+	public final T data;
+
+	public InfoOverlay(OverlayInfo<T> inf) {
+	    this.inf = inf;
+	    this.data = inf.overlay();
+	}
+
+	public void draw(GOut g) {
+	    inf.drawoverlay(g, data);
+	}
+
+	public static <S> InfoOverlay<S> create(OverlayInfo<S> inf) {
+	    return(new InfoOverlay<S>(inf));
+	}
+    }
+
+    public interface NumberInfo extends OverlayInfo<Tex> {
 	public int itemnum();
 	public default Color numcolor() {
 	    return(Color.WHITE);
 	}
+
+	public default Tex overlay() {
+	    return(new TexI(GItem.NumberInfo.numrender(itemnum(), numcolor())));
+	}
+
+	public default void drawoverlay(GOut g, Tex tex) {
+	    g.aimage(tex, g.sz, 1, 1);
+	}
+
 	public static BufferedImage numrender(int num, Color col) {
 	    return(Utils.outline2(Text.render(Integer.toString(num), col).img, Utils.contrast(col)));
 	}
