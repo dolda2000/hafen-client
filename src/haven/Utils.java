bc1488ddb5966a7cd3caea1c15d8191744ef9f31
Fredrik Tolf <fredrik@dolda2000.com>
2018-02-13 01:08:29 +0100

    Removed Utils.myres, has been deprecated for longer than I can remember.

--- src/haven/Utils.java
+++ src/haven/Utils.java
@@ -758,15 +758,6 @@ public class Utils {
 	if(term) out.println();
     }
 
-    public static Resource myres(Class<?> c) {
-	ClassLoader cl = c.getClassLoader();
-	if(cl instanceof Resource.ResClassLoader) {
-	    return(((Resource.ResClassLoader)cl).getres());
-	} else {
-	    return(null);
-	}
-    }
-    
     public static String titlecase(String str) {
 	return(Character.toTitleCase(str.charAt(0)) + str.substring(1));
     }
