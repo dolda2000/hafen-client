14652f892ca9e9173aa9c072562d72d0177efdf5
Fredrik Tolf <fredrik@dolda2000.com>
2018-02-26 03:54:43 +0100

    Made the base Pipe class an interface, and move implementation to BufPipe.

--- src/haven/JOGLPanel.java
+++ src/haven/JOGLPanel.java
@@ -54,7 +54,7 @@ public class JOGLPanel extends GLCanvas implements Runnable, UIPanel {
 
     public JOGLPanel(Coord sz) {
 	super(mkcaps(), null, null, null);
-	base = new Pipe();
+	base = new BufPipe();
 	base.prep(new FragColor(FragColor.defcolor)).prep(new DepthBuffer(DepthBuffer.defdepth));
 	base.prep(new States.Blending());
 	setSize(sz.x, sz.y);
