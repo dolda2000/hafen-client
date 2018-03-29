26c938ec3e8055b9177e96810335fae9a5d62470
Fredrik Tolf <fredrik@dolda2000.com>
2018-03-29 19:46:10 +0200

    Added new vertexbuf format decoding.

--- src/haven/resutil/OverTex.java
+++ src/haven/resutil/OverTex.java
@@ -142,10 +142,16 @@ public class OverTex extends GLState {
 	}
     }
 
-    @VertexBuf.ResName("otex")
+    @VertexBuf.ResName("otex2")
     public static class OTexC extends VertexBuf.Vec2Array {
 	public OTexC(FloatBuffer data) {super(data, otexc);}
-	public OTexC(Resource res, Message buf, int nv) {this(VertexBuf.loadbuf(Utils.wfbuf(nv * 2), buf));}
+	public OTexC(Resource res, Message buf, int nv) {this(VertexBuf.loadbuf2(Utils.wfbuf(nv * 2), buf));}
+    }
+    @VertexBuf.ResName("otex")
+    public static class CDecode implements VertexBuf.ArrayCons {
+	public void cons(Collection<VertexBuf.AttribArray> dst, Resource res, Message buf, int nv) {
+	    dst.add(new OTexC(VertexBuf.loadbuf(Utils.wfbuf(nv * 2), buf)));
+	}
     }
 
     static {
