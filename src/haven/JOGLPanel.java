74088093205d8aa9a52ac85436b4eca89872bf5b
Fredrik Tolf <fredrik@dolda2000.com>
2018-06-16 05:14:33 +0200

    Introduce some debug text in JOGLPanel, if only to make sure FastText is tested.

--- src/haven/JOGLPanel.java
+++ src/haven/JOGLPanel.java
@@ -166,6 +166,12 @@ public class JOGLPanel extends GLCanvas implements Runnable, UIPanel {
 	synchronized(ui) {
 	    ui.draw(g);
 	}
+	if(Config.dbtext) {
+	    int y = g.sz().y;
+	    Runtime rt = Runtime.getRuntime();
+	    long free = rt.freeMemory(), total = rt.totalMemory();
+	    FastText.aprintf(g, new Coord(10, y -= 15), 0, 1, "Mem: %,011d/%,011d/%,011d/%,011d", free, total - free, total, rt.maxMemory());
+	}
     }
 
     public void run() {
