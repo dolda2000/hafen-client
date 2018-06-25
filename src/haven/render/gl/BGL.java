8c98386fe2321f9b43de2db740a9d3e4a6932966
Fredrik Tolf <fredrik@dolda2000.com>
2018-06-25 17:10:33 +0200

    Added bglCallList.

--- src/haven/render/gl/BGL.java
+++ src/haven/render/gl/BGL.java
@@ -105,6 +105,12 @@ public abstract class BGL {
 	    });
     }
 
+    public void bglCallList(final BufferBGL list) {
+	add(new Command() {
+		public void run(GL2 gl) {list.run(gl);}
+	    });
+    }
+
     public void bglCopyBufferf(final FloatBuffer dst, final int doff, final FloatBuffer src, final int soff, final int len) {
 	add(new Command() {
 		public void run(GL2 gl) {
