cdf36f072fa90259f07053a71850efadc1009388
Fredrik Tolf <fredrik@dolda2000.com>
2018-07-29 05:36:39 +0200

    Restored resource material loading and removed lotsa legacy cruft.

--- src/haven/Material.java
+++ src/haven/Material.java
@@ -30,138 +30,19 @@ import java.awt.Color;
 import java.util.*;
 import java.lang.annotation.*;
 import java.lang.reflect.*;
-import javax.media.opengl.*;
-import static haven.Utils.c2fa;
+import haven.render.*;
+import haven.render.States;	// XXXRENDRM
 
-public class Material {
-    // public final GLState[] states; XXXRENDER
-    
-    /* XXXRENDER
-    public static final GLState nofacecull = new GLState.StandAlone(Slot.Type.GEOM, PView.proj) {
-	    public void apply(GOut g) {
-		g.gl.glDisable(GL.GL_CULL_FACE);
-	    }
-	    
-	    public void unapply(GOut g) {
-		g.gl.glEnable(GL.GL_CULL_FACE);
-	    }
-	};
+public class Material implements Pipe.Op {
+    public final Pipe.Op[] states;
+
+    public static final Pipe.Op nofacecull = (p -> p.put(States.facecull, null));
     @ResName("nofacecull")
     public static class $nofacecull implements ResCons {
-	public GLState cons(Resource res, Object... args) {return(nofacecull);}
+	public Pipe.Op cons(Resource res, Object... args) {return(nofacecull);}
     }
-    */
-    
-    public static final float[] defamb = {0.2f, 0.2f, 0.2f, 1.0f};
-    public static final float[] defdif = {0.8f, 0.8f, 0.8f, 1.0f};
-    public static final float[] defspc = {0.0f, 0.0f, 0.0f, 1.0f};
-    public static final float[] defemi = {0.0f, 0.0f, 0.0f, 1.0f};
-    
+
     /* XXXRENDER
-    public static final GLState.Slot<Colors> colors = new GLState.Slot<Colors>(Slot.Type.DRAW, Colors.class);
-    @ResName("col")
-    public static class Colors extends GLState {
-	public float[] amb, dif, spc, emi;
-	public float shine;
-    
-	public Colors() {
-	    amb = defamb;
-	    dif = defdif;
-	    spc = defspc;
-	    emi = defemi;
-	}
-
-	public Colors(float[] amb, float[] dif, float[] spc, float[] emi, float shine) {
-	    this.amb = amb;
-	    this.dif = dif;
-	    this.spc = spc;
-	    this.emi = emi;
-	    this.shine = shine;
-	}
-
-	private static float[] colmul(float[] c1, float[] c2) {
-	    return(new float[] {c1[0] * c2[0], c1[1] * c2[1], c1[2] * c2[2], c1[3] * c2[3]});
-	}
-	
-	private static float[] colblend(float[] in, float[] bl) {
-	    float f1 = bl[3], f2 = 1.0f - f1;
-	    return(new float[] {(in[0] * f2) + (bl[0] * f1),
-				(in[1] * f2) + (bl[1] * f1),
-				(in[2] * f2) + (bl[2] * f1),
-				in[3]});
-	}
-
-	public Colors(Color amb, Color dif, Color spc, Color emi, float shine) {
-	    this(c2fa(amb), c2fa(dif), c2fa(spc), c2fa(emi), shine);
-	}
-	
-	public Colors(Color amb, Color dif, Color spc, Color emi) {
-	    this(amb, dif, spc, emi, 0);
-	}
-	
-	public Colors(Color col) {
-	    this(new Color((int)(col.getRed() * defamb[0]), (int)(col.getGreen() * defamb[1]), (int)(col.getBlue() * defamb[2]), col.getAlpha()),
-		 new Color((int)(col.getRed() * defdif[0]), (int)(col.getGreen() * defdif[1]), (int)(col.getBlue() * defdif[2]), col.getAlpha()),
-		 new Color(0, 0, 0, 0),
-		 new Color(0, 0, 0, 0),
-		 0);
-	}
-    
-	public Colors(Resource res, Object... args) {
-	    this((Color)args[0], (Color)args[1], (Color)args[2], (Color)args[3], (Float)args[4]);
-	}
-
-	public void apply(GOut g) {
-	    BGL gl = g.gl;
-	    gl.glMaterialfv(GL.GL_FRONT_AND_BACK, GL2.GL_AMBIENT, amb, 0);
-	    gl.glMaterialfv(GL.GL_FRONT_AND_BACK, GL2.GL_DIFFUSE, dif, 0);
-	    gl.glMaterialfv(GL.GL_FRONT_AND_BACK, GL2.GL_SPECULAR, spc, 0);
-	    gl.glMaterialfv(GL.GL_FRONT_AND_BACK, GL2.GL_EMISSION, emi, 0);
-	    gl.glMaterialf(GL.GL_FRONT_AND_BACK, GL2.GL_SHININESS, shine);
-	}
-
-	public void unapply(GOut g) {
-	    BGL gl = g.gl;
-	    gl.glMaterialfv(GL.GL_FRONT_AND_BACK, GL2.GL_AMBIENT, defamb, 0);
-	    gl.glMaterialfv(GL.GL_FRONT_AND_BACK, GL2.GL_DIFFUSE, defdif, 0);
-	    gl.glMaterialfv(GL.GL_FRONT_AND_BACK, GL2.GL_SPECULAR, defspc, 0);
-	    gl.glMaterialfv(GL.GL_FRONT_AND_BACK, GL2.GL_EMISSION, defemi, 0);
-	    gl.glMaterialf(GL.GL_FRONT_AND_BACK, GL2.GL_SHININESS, 0.0f);
-	}
-    
-	public int capplyfrom(GLState from) {
-	    if(from instanceof Colors)
-		return(5);
-	    return(-1);
-	}
-
-	public void applyfrom(GOut g, GLState from) {
-	    if(from instanceof Colors)
-		apply(g);
-	}
-	
-	public void prep(Buffer buf) {
-	    Colors p = buf.get(colors);
-	    if(p != null)
-		buf.put(colors, p.combine(this));
-	    else
-		buf.put(colors, this);
-	}
-	
-	public Colors combine(Colors other) {
-	    return(new Colors(colblend(other.amb, this.amb),
-			      colblend(other.dif, this.dif),
-			      colblend(other.spc, this.spc),
-			      colblend(other.emi, this.emi),
-			      other.shine));
-	}
-    
-	public String toString() {
-	    return(String.format("(%.1f, %.1f, %.1f), (%.1f, %.1f, %.1f), (%.1f, %.1f, %.1f @ %.1f)",
-				 amb[0], amb[1], amb[2], dif[0], dif[1], dif[2], spc[0], spc[1], spc[2], shine));
-	}
-    }
-    
     @ResName("order")
     public static class $order implements ResCons {
 	public GLState cons(Resource res, Object... args) {
@@ -185,48 +66,21 @@ public class Material {
 	    }
 	}
     }
-    
-    public void apply(GOut g) {}
-    
-    public void unapply(GOut g) {}
-    
-    public Material(GLState... states) {
+    */
+
+    public Material(Pipe.Op... states) {
 	this.states = states;
     }
-    */
-    public Material(Object... states) {
-	// XXXRENDER
+
+    public String toString() {
+	return(Arrays.asList(states).toString());
     }
 
-    /* XXXRENDER
-    public Material() {
-	this(Light.deflight, new Colors());
+    public void apply(Pipe p) {
+	for(Pipe.Op op : states)
+	    op.apply(p);
     }
-    */
-    
-    public Material(Color amb, Color dif, Color spc, Color emi, float shine) {
-	this(/* XXXRENDER Light.deflight, new Colors(amb, dif, spc, emi, shine) */);
-    }
-    
-    public Material(Color col) {
-	this(/* XXXRENDER Light.deflight, new Colors(col) */);
-    }
-    
-    public Material(Tex tex) {
-	this(/* XXXRENDER Light.deflight, new Colors(), tex.draw(), tex.clip() */);
-    }
-    
-    public String toString() {
-	return(Arrays.asList(/* XXXRENDER states */).toString());
-    }
-    
-    /* XXXRENDER
-    public void prep(Buffer buf) {
-	for(GLState st : states)
-	    st.prep(buf);
-    }
-    */
-    
+
     public interface Owner extends OwnerContext {
     }
 
@@ -269,29 +123,28 @@ public class Material {
 
     public static class Res extends Resource.Layer implements Resource.IDLayer<Integer> {
 	public final int id;
-	// private transient List<GLState> states = new LinkedList<GLState>(); XXXRENDER
-	private transient List<Resolver> left = new LinkedList<Resolver>();
+	private transient List<Pipe.Op> states = new LinkedList<>();
+	private transient List<Resolver> left = new LinkedList<>();
 	private transient Material m;
-	private boolean mipmap = false, linear = false;
-	
+
 	public interface Resolver {
-	    public void resolve(Collection<Object> buf); // XXXRENDER
+	    public void resolve(Collection<Pipe.Op> buf);
 	}
-	
+
 	public Res(Resource res, int id) {
 	    res.super();
 	    this.id = id;
 	}
-	
+
 	public Material get() {
 	    synchronized(this) {
 		if(m == null) {
 		    for(Iterator<Resolver> i = left.iterator(); i.hasNext();) {
 			Resolver r = i.next();
-			// r.resolve(states); XXXRENDER
+			r.resolve(states);
 			i.remove();
 		    }
-		    m = new Material(/* XXXRENDER states.toArray(new GLState[0]) */) {
+		    m = new Material(states.toArray(new Pipe.Op[0])) {
 			    public String toString() {
 				return(super.toString() + "@" + getres().name);
 			    }
@@ -301,104 +154,12 @@ public class Material {
 	    }
 	}
 
-	public void init() {
-	    for(Resource.Image img : getres().layers(Resource.imgc)) {
-		/* XXXRENDER
-		TexGL tex = (TexGL)img.tex();
-		if(mipmap)
-		    tex.mipmap();
-		if(linear)
-		    tex.magfilter(GL.GL_LINEAR);
-		*/
-	    }
-	}
-	
+	public void init() {}
+
 	public Integer layerid() {
 	    return(id);
 	}
     }
-
-    /* XXXRENDER
-    @Resource.LayerName("mat")
-    public static class OldMat implements Resource.LayerFactory<Res> {
-	private static Color col(Message buf) {
-	    return(new Color((int)(buf.cpfloat() * 255.0),
-			     (int)(buf.cpfloat() * 255.0),
-			     (int)(buf.cpfloat() * 255.0),
-			     (int)(buf.cpfloat() * 255.0)));
-	}
-
-	public Res cons(final Resource res, Message buf) {
-	    int id = buf.uint16();
-	    Res ret = new Res(res, id);
-	    GLState light = Light.deflight;
-	    while(!buf.eom()) {
-		String thing = buf.string().intern();
-		if(thing == "col") {
-		    Color amb = col(buf);
-		    Color dif = col(buf);
-		    Color spc = col(buf);
-		    double shine = buf.cpfloat();
-		    Color emi = col(buf);
-		    ret.states.add(new Colors(amb, dif, spc, emi, (float)shine));
-		} else if(thing == "linear") {
-		    ret.linear = true;
-		} else if(thing == "mipmap") {
-		    ret.mipmap = true;
-		} else if(thing == "nofacecull") {
-		    ret.states.add(nofacecull);
-		} else if(thing == "tex") {
-		    final int tid = buf.uint16();
-		    ret.left.add(new Res.Resolver() {
-			    public void resolve(Collection<GLState> buf) {
-				for(Resource.Image img : res.layers(Resource.imgc)) {
-				    if(img.id == tid) {
-					buf.add(img.tex().draw());
-					buf.add(img.tex().clip());
-					return;
-				    }
-				}
-				throw(new RuntimeException(String.format("Specified texture %d not found in %s", tid, res)));
-			    }
-			});
-		} else if(thing == "texlink") {
-		    final String nm = buf.string();
-		    final int ver = buf.uint16();
-		    final int tid = buf.uint16();
-		    ret.left.add(new Res.Resolver() {
-			    public void resolve(Collection<GLState> buf) {
-				Indir<Resource> tres = res.pool.load(nm, ver);
-				for(Resource.Image img : tres.get().layers(Resource.imgc)) {
-				    if(img.id == tid) {
-					buf.add(img.tex().draw());
-					buf.add(img.tex().clip());
-					return;
-				    }
-				}
-				throw(new RuntimeException(String.format("Specified texture %d for %s not found in %s", tid, res, tres)));
-			    }
-			});
-		} else if(thing == "light") {
-		    String l = buf.string();
-		    if(l.equals("pv")) {
-			light = Light.vlights;
-		    } else if(l.equals("pp")) {
-			light = Light.plights;
-		    } else if(l.equals("n")) {
-			light = null;
-		    } else {
-			throw(new Resource.LoadException("Unknown lighting type: " + thing, res));
-		    }
-		} else {
-		    throw(new Resource.LoadException("Unknown material part: " + thing, res));
-		}
-	    }
-	    if(light != null)
-		ret.states.add(light);
-	    return(ret);
-	}
-    }
-    */
 
     @ResName("mlink")
     public static class $mlink implements ResCons2 {
@@ -406,7 +167,7 @@ public class Material {
 	    final Indir<Resource> lres = res.pool.load((String)args[0], (Integer)args[1]);
 	    final int id = (args.length > 2)?(Integer)args[2]:-1;
 	    return(new Res.Resolver() {
-		    public void resolve(Collection<Object> buf) {
+		    public void resolve(Collection<Pipe.Op> buf) {
 			if(id >= 0) {
 			    Res mat = lres.get().layer(Res.class, id);
 			    if(mat == null)
@@ -431,7 +192,7 @@ public class Material {
     }
 
     public interface ResCons {
-	public /* XXXRENDER GLState */ Object cons(Resource res, Object... args);
+	public Pipe.Op cons(Resource res, Object... args);
     }
 
     public interface ResCons2 {
@@ -454,13 +215,11 @@ public class Material {
 		}
 		rnames.put(nm, new ResCons2() {
 			public Res.Resolver cons(Resource res, Object... args) {
-			    // final GLState ret = scons.cons(res, args); XXXRENDER
+			    final Pipe.Op ret = scons.cons(res, args);
 			    return(new Res.Resolver() {
-				    public void resolve(Collection<Object> buf) {
-					/* XXXRENDER
+				    public void resolve(Collection<Pipe.Op> buf) {
 					if(ret != null)
 					    buf.add(ret);
-					*/
 				    }
 				});
 			}
@@ -473,23 +232,23 @@ public class Material {
 		} catch(IllegalAccessException e) {
 		    throw(new Error(e));
 		}
-	    } /* else if(GLState.class.isAssignableFrom(cl)) { XXXRENDER
-		final Constructor<? extends GLState> cons;
+	    } else if(Pipe.Op.class.isAssignableFrom(cl)) {
+		Constructor<? extends Pipe.Op> cons;
 		try {
-		    cons = cl.asSubclass(GLState.class).getConstructor(Resource.class, Object[].class);
+		    cons = cl.asSubclass(Pipe.Op.class).getConstructor(Resource.class, Object[].class);
 		} catch(NoSuchMethodException e) {
 		    throw(new Error("No proper constructor for res-consable GL state " + cl.getName(), e));
 		}
 		rnames.put(nm, new ResCons2() {
-			public Res.Resolver cons(final Resource res, final Object... args) {
+			public Res.Resolver cons(Resource res, Object... args) {
 			    return(new Res.Resolver() {
-				    public void resolve(Collection<GLState> buf) {
+				    public void resolve(Collection<Pipe.Op> buf) {
 					buf.add(Utils.construct(cons, res, args));
 				    }
 				});
 			}
 		    });
-	    } */ else {
+	    } else {
 		throw(new Error("Illegal material constructor class: " + cl));
 	    }
 	}
@@ -503,19 +262,10 @@ public class Material {
 	    while(!buf.eom()) {
 		String nm = buf.string();
 		Object[] args = buf.list();
-		if(nm.equals("linear")) {
-		    /* XXX: These should very much be removed and
-		     * specified directly in the texture layer
-		     * instead. */
-		    ret.linear = true;
-		} else if(nm.equals("mipmap")) {
-		    ret.mipmap = true;
-		} else {
-		    ResCons2 cons = rnames.get(nm);
-		    if(cons == null)
-			throw(new Resource.LoadException("Unknown material part name: " + nm, res));
-		    ret.left.add(cons.cons(res, args));
-		}
+		ResCons2 cons = rnames.get(nm);
+		if(cons == null)
+		    throw(new Resource.LoadException("Unknown material part name: " + nm, res));
+		ret.left.add(cons.cons(res, args));
 	    }
 	    return(ret);
 	}
