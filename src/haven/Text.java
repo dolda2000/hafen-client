a84f1a7a2800d924e3e027dc3538aae6e8f73886
Fredrik Tolf <fredrik@dolda2000.com>
2018-09-10 21:39:47 +0200

    Made UText more flexible.

--- src/haven/Text.java
+++ src/haven/Text.java
@@ -204,13 +204,14 @@ public class Text {
 
 	public UText(Furnace fnd) {this.fnd = fnd;}
 
+	protected Text render(String text) {return(fnd.render(text));}
 	protected String text(T value) {return(String.valueOf(value));}
 	protected abstract T value();
 
 	public Text get() {
 	    T value = value();
 	    if(!Utils.eq(value, cv))
-		cur = fnd.render(text(cv = value));
+		cur = render(text(cv = value));
 	    return(cur);
 	}
 
