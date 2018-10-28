81c86875bf74e1586370c3feb526283a6c048e4e
Fredrik Tolf <fredrik@dolda2000.com>
2018-10-28 23:12:00 +0100

    Added RUtils for potentially common rendering tasks.

---
+++ src/haven/RUtils.java
@@ -0,0 +1,59 @@
+/*
+ *  This file is part of the Haven & Hearth game client.
+ *  Copyright (C) 2009 Fredrik Tolf <fredrik@dolda2000.com>, and
+ *                     Björn Johannessen <johannessen.bjorn@gmail.com>
+ *
+ *  Redistribution and/or modification of this file is subject to the
+ *  terms of the GNU Lesser General Public License, version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  Other parts of this source tree adhere to other copying
+ *  rights. Please see the file `COPYING' in the root directory of the
+ *  source tree for details.
+ *
+ *  A copy the GNU Lesser General Public License is distributed along
+ *  with the source tree of which this file is a part in the file
+ *  `doc/LPGL-3'. If it is missing for any reason, please see the Free
+ *  Software Foundation's website at <http://www.fsf.org/>, or write
+ *  to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *  Boston, MA 02111-1307 USA
+ */
+
+package haven;
+
+import java.util.*;
+import java.util.function.*;
+import haven.render.*;
+import haven.render.RenderTree.Node;
+import haven.render.RenderTree.Slot;
+
+public class RUtils {
+    public static void readd(Collection<Slot> slots, Consumer<Slot> add, Runnable revert) {
+	Collection<Slot> ch = new ArrayList<>(slots.size());
+	try {
+	    for(Slot slot : slots) {
+		ch.add(slot);
+		slot.clear();
+		add.accept(slot);
+	    }
+	} catch(RuntimeException e) {
+	    revert.run();
+	    try {
+		for(Slot slot : ch) {
+		    slot.clear();
+		    add.accept(slot);
+		}
+	    } catch(RuntimeException e2) {
+		Error err = new Error("Unexpected non-local exit", e2);
+		err.addSuppressed(e);
+		throw(err);
+	    }
+	    throw(e);
+	}
+    }
+}
