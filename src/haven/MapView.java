00bd4f77f8ea2c92c15e5c81c5cd90ea47703d00
Fredrik Tolf <fredrik@dolda2000.com>
2018-11-03 06:15:20 +0100

    Fixed clicklist update bug.

--- src/haven/MapView.java
+++ src/haven/MapView.java
@@ -826,11 +826,13 @@ public class MapView extends PView implements DTarget, Console.Directory {
 	public class Clickslot implements Slot<Rendered> {
 	    public final Slot<? extends Rendered> bk;
 	    public final int id;
+	    final Pipe idp;
 	    private GroupPipe state;
 
 	    public Clickslot(Slot<? extends Rendered> bk, int id) {
 		this.bk = bk;
 		this.id = id;
+		this.idp = new SinglePipe<>(FragID.id, new FragID.ID(id2col(id)));
 	    }
 
 	    public Rendered obj() {
@@ -845,12 +847,10 @@ public class MapView extends PView implements DTarget, Console.Directory {
 
 	    private class IDState implements GroupPipe {
 		static final int idx_bas = 0, idx_idp = 1, idx_back = 2;
-		final Pipe idp;
 		final GroupPipe back;
 
 		IDState(GroupPipe back) {
 		    this.back = back;
-		    this.idp = new SinglePipe<>(FragID.id, new FragID.ID(id2col(id)));
 		}
 
 		public Pipe group(int idx) {
@@ -912,8 +912,10 @@ public class MapView extends PView implements DTarget, Console.Directory {
 	public void update(Slot<? extends Rendered> slot) {
 	    if(back != null) {
 		Clickslot cs = slots.get(slot);
-		if(cs != null)
+		if(cs != null) {
+		    cs.state = null;
 		    back.update(cs);
+		}
 	    }
 	}
 
