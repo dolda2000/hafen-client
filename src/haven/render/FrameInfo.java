e08d174251c7bb9370016799e026c1739dee92f1
Fredrik Tolf <fredrik@dolda2000.com>
2018-11-18 06:24:04 +0100

    WIP: Restoring water tiles.

---
+++ src/haven/render/FrameInfo.java
@@ -0,0 +1,54 @@
+/*
+ *  This file is part of the Haven & Hearth game client.
+ *  Copyright (C) 2009 Fredrik Tolf <fredrik@dolda2000.com>, and
+ *                     Björn Johannessen <johannessen.bjorn@gmail.com>
+ *
+ *  Redistribution and/or modification of this file is subject to the
+ *  terms of the GNU Lesser General Public License, version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  Other parts of this source tree adhere to other copying
+ *  rights. Please see the file `COPYING' in the root directory of the
+ *  source tree for details.
+ *
+ *  A copy the GNU Lesser General Public License is distributed along
+ *  with the source tree of which this file is a part in the file
+ *  `doc/LPGL-3'. If it is missing for any reason, please see the Free
+ *  Software Foundation's website at <http://www.fsf.org/>, or write
+ *  to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *  Boston, MA 02111-1307 USA
+ */
+
+package haven.render;
+
+import haven.*;
+import haven.render.sl.*;
+
+public class FrameInfo extends State {
+    public static final Slot<FrameInfo> slot = new Slot<>(Slot.Type.SYS, FrameInfo.class);
+    public static final Uniform u_time = new Uniform(Type.FLOAT, "time", p -> {
+	    FrameInfo inf = p.get(slot);
+	    return((inf == null) ? 0.0f : (float)inf.time);
+	}, slot);
+    public final double time;
+
+    public FrameInfo(double time) {
+	this.time = time;
+    }
+
+    public FrameInfo() {
+	this(Utils.rtime());
+    }
+
+    public ShaderMacro shader() {return(null);}
+    public void apply(Pipe p) {p.put(slot, this);}
+
+    public static Expression time() {
+	return(u_time.ref());
+    }
+}
