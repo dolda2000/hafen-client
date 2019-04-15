44bd5019db0a3aef6f0a3cd5c4eae3545e84f888
Fredrik Tolf <fredrik@dolda2000.com>
2019-04-16 01:33:57 +0200

    WIP: Adding render-context for post-processing and other purposes.

---
+++ src/haven/RenderContext.java
@@ -0,0 +1,55 @@
+/*
+ *  This file is part of the Haven & Hearth game client.
+ *  Copyright (C) 2009 Fredrik Tolf <fredrik@dolda2000.com>, and
+ *                     Björn Johannessen <johannessen.bjorn@gmail.com>
+ *
+ *  Redistribution and/or modification of this file is subject to the
+ *  terms of the GNU Lesser General Public License, version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  Other parts of this source tree adhere to other copying
+ *  rights. Please see the file `COPYING' in the root directory of the
+ *  source tree for details.
+ *
+ *  A copy the GNU Lesser General Public License is distributed along
+ *  with the source tree of which this file is a part in the file
+ *  `doc/LPGL-3'. If it is missing for any reason, please see the Free
+ *  Software Foundation's website at <http://www.fsf.org/>, or write
+ *  to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *  Boston, MA 02111-1307 USA
+ */
+
+package haven;
+
+import java.util.*;
+import haven.render.*;
+import haven.render.sl.ShaderMacro;
+import haven.render.Texture2D.Sampler2D;
+
+public class RenderContext extends State {
+    public static final Slot<RenderContext> slot = new Slot<>(Slot.Type.SYS, RenderContext.class);
+    private final Collection<PostProcessor> post = new ArrayList<>();
+
+    public static abstract class PostProcessor implements Disposable {
+	public Sampler2D buf = null;
+
+	public abstract void run(GOut g, Sampler2D in);
+
+	public void dispose() {
+	    if(buf != null)
+		buf.dispose();
+	}
+    }
+
+    public Iterable<PostProcessor> postproc() {
+	return(post);
+    }
+
+    public ShaderMacro shader() {return(null);}
+    public void apply(Pipe p) {p.put(slot, this);}
+}
