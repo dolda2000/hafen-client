d8b29e67ddba79431acd0f04665298d7c15618b7
Fredrik Tolf <fredrik@dolda2000.com>
2019-08-26 17:25:56 +0200

    Fixed up support for mat3/mat4 attributes.

--- src/haven/render/gl/GLVertexArray.java
+++ src/haven/render/gl/GLVertexArray.java
@@ -116,22 +116,34 @@ public class GLVertexArray extends GLObject implements BGL.ID {
 		    GLProgram.VarID var = prog.cattrib(attr.tgt);
 		    if(var != null) {
 			VboState.apply(gl, g.state, bufs[attr.buf]);
-			gl.glVertexAttribPointer(var, attr.el.nc, GLRender.glattribfmt(attr.el.cf), GLRender.glattribnorm(attr.el.cf), attr.stride, attr.offset);
-			if(attr.instanced) {
-			    if(attr.tgt.type == Type.MAT4) {
-				gl.glVertexAttribDivisor(var, 0, 1);
-				gl.glVertexAttribDivisor(var, 1, 1);
-				gl.glVertexAttribDivisor(var, 2, 1);
-				gl.glVertexAttribDivisor(var, 3, 1);
-			    } else if(attr.tgt.type == Type.MAT3) {
-				gl.glVertexAttribDivisor(var, 0, 1);
-				gl.glVertexAttribDivisor(var, 1, 1);
-				gl.glVertexAttribDivisor(var, 2, 1);
-			    } else {
+			int na = 1, vo = 0;
+			VectorFormat vfmt = attr.el;
+			if(attr.tgt.type == Type.MAT4) {
+			    if((attr.el.nc != 16) || (attr.el.cf != NumberFormat.FLOAT32))
+				throw(new RuntimeException("unexpected mat4 vertex format: " + attr.el));
+			    na = 4;
+			    vo = 16;
+			    vfmt = new VectorFormat(4, NumberFormat.FLOAT32);
+			} else if(attr.tgt.type == Type.MAT3) {
+			    if((attr.el.nc != 9) || (attr.el.cf != NumberFormat.FLOAT32))
+				throw(new RuntimeException("unexpected mat3 vertex format: " + attr.el));
+			    na = 3;
+			    vo = 12;
+			    vfmt = new VectorFormat(3, NumberFormat.FLOAT32);
+			}
+			if(na == 1) {
+			    gl.glVertexAttribPointer(var, attr.el.nc, GLRender.glattribfmt(attr.el.cf), GLRender.glattribnorm(attr.el.cf), attr.stride, attr.offset);
+			    gl.glEnableVertexAttribArray(var);
+			    if(attr.instanced)
 				gl.glVertexAttribDivisor(var, 1);
+			} else {
+			    for(int v = 0; i < na; i++) {
+				gl.glVertexAttribPointer(var, v, vfmt.nc, GLRender.glattribfmt(vfmt.cf), GLRender.glattribnorm(vfmt.cf), attr.stride, attr.offset + (v * vo));
+				gl.glEnableVertexAttribArray(var, v);
+				if(attr.instanced)
+				    gl.glVertexAttribDivisor(var, v, 1);
 			    }
 			}
-			gl.glEnableVertexAttribArray(var);
 		    }
 		}
 	    });
