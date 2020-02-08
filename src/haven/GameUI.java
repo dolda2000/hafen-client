88a0bd2c19dc31670e71b7446be25ec66ab02b3f
Fredrik Tolf <fredrik@dolda2000.com>
2020-02-08 02:04:55 +0100

    Clear console output as well on GameUI destruction.

--- src/haven/GameUI.java
+++ src/haven/GameUI.java
@@ -164,6 +164,7 @@ public class GameUI extends ConsoleHost implements Console.Directory {
 
     public void dispose() {
 	Debug.log = new java.io.PrintWriter(System.err);
+	ui.cons.clearout();
 	super.dispose();
     }
     
