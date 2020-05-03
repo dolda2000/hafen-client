da73d05db4e14f62a13b9be7251a4ad439228da2
Fredrik Tolf <fredrik@dolda2000.com>
2020-05-04 00:58:49 +0200

    Make gprefs part of the conf settings rather than the frame settings.

--- src/haven/PView.java
+++ src/haven/PView.java
@@ -88,7 +88,7 @@ public abstract class PView extends Widget {
     private final WidgetContext ctx = new WidgetContext(this);
     private Pipe.Op conf() {
 	rsz = rendersz();
-	return(Pipe.Op.compose(new FrameConfig(rsz), ctx));
+	return(Pipe.Op.compose(new FrameConfig(rsz), ctx, curprefs));
     }
 
     private Pipe.Op curconf = null;
@@ -98,6 +98,7 @@ public abstract class PView extends Widget {
 	return(curconf);
     }
 
+    private GSettings curprefs = null;
     protected GSettings gprefs() {
 	if(ui == null)
 	    return(null);
@@ -106,7 +107,6 @@ public abstract class PView extends Widget {
 
     private Pipe.Op frame() {
 	return(Pipe.Op.compose(curconf(),
-			       gprefs(),
 			       new FrameInfo(),
 			       ((ui == null) || (ui.sess == null)) ? null : new Glob.FrameInfo(ui.sess.glob)));
     }
@@ -168,6 +168,11 @@ public abstract class PView extends Widget {
 
     public void tick(double dt) {
 	super.tick(dt);
+	GSettings gprefs = gprefs();
+	if(gprefs != this.curprefs) {
+	    this.curprefs = gprefs;
+	    reconf();
+	}
 	conf.ostate(frame());
 	ticklist.tick(dt);
 	if(audio != null)
