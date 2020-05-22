1681b461d10997db27a40d52732bf7a0aa2df46d
Fredrik Tolf <fredrik@dolda2000.com>
2020-05-22 18:05:49 +0200

    Add a render-profile tick for ticking now that it can be had.

--- src/haven/JOGLPanel.java
+++ src/haven/JOGLPanel.java
@@ -451,6 +451,7 @@ public class JOGLPanel extends GLCanvas implements Runnable, UIPanel, Console.Di
 			if((ui.root.sz.x != (shape.br.x - shape.ul.x)) || (ui.root.sz.y != (shape.br.y - shape.ul.y)))
 			    ui.root.resize(new Coord(shape.br.x - shape.ul.x, shape.br.y - shape.ul.y));
 			if(curf != null) curf.tick("tick");
+			buf.submit(new ProfileTick(rprofc, "tick"));
 			if(curgf != null) curgf.tick(buf, "tick");
 		    }
 
