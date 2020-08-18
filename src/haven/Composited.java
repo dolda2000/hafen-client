9dbde9a5dfae229e4b1b855ccf6a8914659db8b0
Fredrik Tolf <fredrik@dolda2000.com>
2020-08-18 22:22:42 +0200

    Add depth-bias to composited meshes.

--- src/haven/Composited.java
+++ src/haven/Composited.java
@@ -158,7 +158,12 @@ public class Composited implements RenderTree.Node {
 	    }
 
 	    public void added(RenderTree.Slot slot) {
-		slot.ostate(Pipe.Op.compose(mat, order, (order.z2 == 0) ? null : (p -> p.put(Clickable.slot, null))));
+		float z = Model.this.z;
+		/* XXX: The depth-bias should not be necessary, but
+		 * without it Z-fighting between meshes appears to
+		 * occur for unclear reasons. GLSL variance due to
+		 * different... bone indices? */
+		slot.ostate(Pipe.Op.compose(mat, new States.DepthBias(-z, -z), order, (order.z2 == 0) ? null : (p -> p.put(Clickable.slot, null))));
 		slot.lockstate();
 		slot.add(m);
 	    }
