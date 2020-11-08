a900062dc5655ae4e5633745ac63dad4ec309f9d
Fredrik Tolf <fredrik@dolda2000.com>
2020-11-08 03:52:16 +0100

    Include shift on forchar's modmask, because I don't see why not.

--- src/haven/KeyMatch.java
+++ src/haven/KeyMatch.java
@@ -118,7 +118,7 @@ public class KeyMatch {
 	return(new KeyMatch(chr, false, VK_UNDEFINED, false, Character.toString(chr), modmask, modmatch));
     }
     public static KeyMatch forchar(char chr, int mods) {
-	return(forchar(chr, C | M, mods));
+	return(forchar(chr, S | C | M, mods));
     }
 
     public static KeyMatch forcode(int code, int modmask, int modmatch) {
