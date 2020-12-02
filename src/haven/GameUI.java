627d7a33340f379a5ca856c3d79de4c5040cb5e0
Fredrik Tolf <fredrik@dolda2000.com>
2020-12-02 23:07:33 +0100

    Replaced menu-buttons with checkboxes.

--- src/haven/GameUI.java
+++ src/haven/GameUI.java
@@ -1144,6 +1144,12 @@ public class GameUI extends ConsoleHost implements Console.Directory {
 	wdg.c = fitwdg(wdg, wdg.c);
     }
 
+    private boolean wndstate(Window wnd) {
+	if(wnd == null)
+	    return(false);
+	return(wnd.visible);
+    }
+
     private void togglewnd(Window wnd) {
 	if(wnd != null) {
 	    if(wnd.show(!wnd.visible)) {
@@ -1179,11 +1185,11 @@ public class GameUI extends ConsoleHost implements Console.Directory {
     public class MainMenu extends Widget {
 	public MainMenu() {
 	    super(menubg.sz());
-	    add(new MenuButton("rbtn-inv", kb_inv, "Inventory"), 0, 0).action(() -> togglewnd(invwnd));
-	    add(new MenuButton("rbtn-equ", kb_equ, "Equipment"), 0, 0).action(() -> togglewnd(equwnd));
-	    add(new MenuButton("rbtn-chr", kb_chr, "Character Sheet"), 0, 0).action(() -> togglewnd(chrwdg));
-	    add(new MenuButton("rbtn-bud", kb_bud, "Kith & Kin"), 0, 0).action(() -> togglewnd(zerg));
-	    add(new MenuButton("rbtn-opt", kb_opt, "Options"), 0, 0).action(() -> togglewnd(opts));
+	    add(new MenuCheckBox("rbtn-inv", kb_inv, "Inventory"), 0, 0).state(() -> wndstate(invwnd)).click(() -> togglewnd(invwnd));
+	    add(new MenuCheckBox("rbtn-equ", kb_equ, "Equipment"), 0, 0).state(() -> wndstate(equwnd)).click(() -> togglewnd(equwnd));
+	    add(new MenuCheckBox("rbtn-chr", kb_chr, "Character Sheet"), 0, 0).state(() -> wndstate(chrwdg)).click(() -> togglewnd(chrwdg));
+	    add(new MenuCheckBox("rbtn-bud", kb_bud, "Kith & Kin"), 0, 0).state(() -> wndstate(zerg)).click(() -> togglewnd(zerg));
+	    add(new MenuCheckBox("rbtn-opt", kb_opt, "Options"), 0, 0).state(() -> wndstate(opts)).click(() -> togglewnd(opts));
 	}
 
 	public void draw(GOut g) {
@@ -1214,12 +1220,12 @@ public class GameUI extends ConsoleHost implements Console.Directory {
 	    add(new MenuCheckBox("lbtn-claim", kb_claim, "Display personal claims"), 0, 0).changed(a -> toggleol(0, a));
 	    add(new MenuCheckBox("lbtn-vil", kb_vil, "Display village claims"), 0, 0).changed(a -> toggleol(2, a));
 	    add(new MenuCheckBox("lbtn-rlm", kb_rlm, "Display realms"), 0, 0).changed(a -> toggleol(4, a));
-	    add(new MenuButton("lbtn-map", kb_map, "Map")).action(() -> {
+	    add(new MenuCheckBox("lbtn-map", kb_map, "Map")).state(() -> wndstate(mapfile)).click(() -> {
 		    togglewnd(mapfile);
 		    if(mapfile != null)
 			Utils.setprefb("wndvis-map", mapfile.visible);
 		});
-	    add(new MenuButton("lbtn-ico", kb_ico, "Icon settings"), 0, 0).action(() -> {
+	    add(new MenuCheckBox("lbtn-ico", kb_ico, "Icon settings"), 0, 0).state(() -> wndstate(iconwnd)).click(() -> {
 		    if(iconconf == null)
 			return;
 		    if(iconwnd == null) {
