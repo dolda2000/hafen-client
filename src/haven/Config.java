ba92b46417e7947028fee991584a568cf58b4fbb
Fredrik Tolf <fredrik@dolda2000.com>
2021-03-25 01:54:35 +0100

    Added support for launcher-embedded token authentication.

--- src/haven/Config.java
+++ src/haven/Config.java
@@ -53,7 +53,7 @@ public class Config {
     public static int authport = getint("haven.authport", 1871);
     public static boolean softres = getprop("haven.softres", "on").equals("on");
     public static Double uiscale = getfloat("haven.uiscale", null);
-    public static byte[] authck = null;
+    public static byte[] authck = null, inittoken = null;
     public static String prefspec = "hafen";
     public static final String confid = "";
     
@@ -61,6 +61,8 @@ public class Config {
 	String p;
 	if((p = getprop("haven.authck", null)) != null)
 	    authck = Utils.hex2byte(p);
+	if((p = getprop("haven.inittoken", null)) != null)
+	    inittoken = Utils.hex2byte(p);
     }
     
     private static int getint(String name, int def) {
