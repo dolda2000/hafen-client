28d6f0ab67c2c06dca18f8f67ee6636c9b824ac5
Fredrik Tolf <fredrik@dolda2000.com>
2021-07-04 16:16:38 +0200

    Add a function for fetching text-entry contents, and deprecate the text variable.

--- src/haven/TextEntry.java
+++ src/haven/TextEntry.java
@@ -44,7 +44,7 @@ public class TextEntry extends SIWidget {
     public LineEdit buf;
     public int sx;
     public boolean pw = false;
-    public String text;
+    @Deprecated public String text;
     private boolean dirty = false;
     private double focusstart;
     private Text.Line tcache = null;
@@ -181,5 +181,9 @@ public class TextEntry extends SIWidget {
     public void resize(int w) {
 	resize(w, sz.y);
 	redraw();
+    }
+
+    public String text() {
+	return(this.text);
     }
 }
