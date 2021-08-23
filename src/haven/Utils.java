59507a137ad03118dc1f0aa11612d040e46917ee
Fredrik Tolf <fredrik@dolda2000.com>
2021-08-23 21:05:00 +0200

    Improved whitespacing in Utils a bit.

--- src/haven/Utils.java
+++ src/haven/Utils.java
@@ -66,7 +66,7 @@ public class Utils {
 		}
 	    });
     }
-	
+
     static void drawgay(BufferedImage t, BufferedImage img, Coord c) {
 	Coord sz = imgsz(img);
 	for(int y = 0; y < sz.y; y++) {
@@ -111,19 +111,19 @@ public class Utils {
 	g.drawString(text, c.x, c.y + m.getAscent());
 	return(m.getHeight());
     }
-	
+
     static Coord textsz(Graphics g, String text) {
 	java.awt.FontMetrics m = g.getFontMetrics();
 	java.awt.geom.Rectangle2D ts = m.getStringBounds(text, g);
 	return(new Coord((int)ts.getWidth(), (int)ts.getHeight()));
     }
-	
+
     static void aligntext(Graphics g, String text, Coord c, double ax, double ay) {
 	java.awt.FontMetrics m = g.getFontMetrics();
 	java.awt.geom.Rectangle2D ts = m.getStringBounds(text, g);
 	g.drawString(text, (int)(c.x - ts.getWidth() * ax), (int)(c.y + m.getAscent() - ts.getHeight() * ay));
     }
-    
+
     public static String fpformat(int num, int div, int dec) {
 	StringBuilder buf = new StringBuilder();
 	boolean s = false;
@@ -174,7 +174,7 @@ public class Utils {
 	buf.append(dp);
 	return(buf.toString());
     }
-    
+
     public static String odformat2(double num, int md) {
 	if(num < 0)
 	    return("-" + odformat2(-num, md));
@@ -203,7 +203,7 @@ public class Utils {
     static void line(Graphics g, Coord c1, Coord c2) {
 	g.drawLine(c1.x, c1.y, c2.x, c2.y);
     }
-	
+
     static void AA(Graphics g) {
 	java.awt.Graphics2D g2 = (java.awt.Graphics2D)g;
 	g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);		
@@ -329,7 +329,7 @@ public class Utils {
 	    return(def);
 	}
     }
-	
+
     public static void setprefb(String prefname, byte[] val) {
 	try {
 	    prefs().putByteArray(prefname, val);
@@ -406,22 +406,22 @@ public class Utils {
     public static int uint16d(byte[] buf, int off) {
 	return(ub(buf[off]) | (ub(buf[off + 1]) << 8));
     }
-	
+
     public static int int16d(byte[] buf, int off) {
 	return((int)(short)uint16d(buf, off));
     }
-	
+
     public static long uint32d(byte[] buf, int off) {
 	return((long)ub(buf[off]) | ((long)ub(buf[off + 1]) << 8) | ((long)ub(buf[off + 2]) << 16) | ((long)ub(buf[off + 3]) << 24));
     }
-	
+
     public static void uint32e(long num, byte[] buf, int off) {
 	buf[off] = (byte)(num & 0xff);
 	buf[off + 1] = (byte)((num & 0x0000ff00) >> 8);
 	buf[off + 2] = (byte)((num & 0x00ff0000) >> 16);
 	buf[off + 3] = (byte)((num & 0xff000000) >> 24);
     }
-	
+
     public static int int32d(byte[] buf, int off) {
 	return((int)uint32d(buf, off));
     }
@@ -479,7 +479,7 @@ public class Utils {
 	off[0] = i + 1;
 	return(ret);
     }
-    
+
     public static double floatd(byte[] buf, int off) {
 	int e = buf[off];
 	long t = uint32d(buf, off + 1);
@@ -652,7 +652,7 @@ public class Utils {
 	else
 	    return((char)('A' + num - 10));
     }
-	
+
     static int hex2num(char hex) {
 	if((hex >= '0') && (hex <= '9'))
 	    return(hex - '0');
@@ -681,7 +681,7 @@ public class Utils {
 	    ret[o] = (byte)((hex2num(hex.charAt(i)) << 4) | hex2num(hex.charAt(i + 1)));
 	return(ret);
     }
-    
+
     private final static String base64set = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
     private final static int[] base64rev;
     static {
@@ -736,7 +736,7 @@ public class Utils {
 	}
 	return(buf.toByteArray());
     }
-	
+
     public static String[] splitwords(String text) {
 	ArrayList<String> words = new ArrayList<String>();
 	StringBuilder buf = new StringBuilder();
@@ -791,7 +791,7 @@ public class Utils {
 	    return(null);
 	return(words.toArray(new String[0]));
     }
-	
+
     public static String[] splitlines(String text) {
 	ArrayList<String> ret = new ArrayList<String>();
 	int p = 0;
@@ -814,7 +814,7 @@ public class Utils {
 	    return(0);
 	}
     }
-    
+
     static void readtileof(InputStream in) throws IOException {
         byte[] buf = new byte[4096];
         while(true) {
@@ -822,7 +822,7 @@ public class Utils {
                 return;
         }
     }
-    
+
     public static byte[] readall(InputStream in) throws IOException {
 	byte[] buf = new byte[4096];
 	int off = 0;
@@ -1023,7 +1023,7 @@ public class Utils {
     public static String titlecase(String str) {
 	return(Character.toTitleCase(str.charAt(0)) + str.substring(1));
     }
-    
+
     public static Color contrast(Color col) {
 	int max = Math.max(col.getRed(), Math.max(col.getGreen(), col.getBlue()));
 	if(max > 128) {
@@ -1054,7 +1054,7 @@ public class Utils {
                          ((col & 0x00f0) >>  4) * 17,
                          ((col & 0x000f) >>  0) * 17));
     }
-    
+
     public static BufferedImage outline(BufferedImage img, Color col) {
 	Coord sz = imgsz(img).add(2, 2);
 	BufferedImage ol = TexI.mkbuf(sz);
@@ -1080,7 +1080,7 @@ public class Utils {
 	}
 	return(ol);
     }
-    
+
     public static BufferedImage outline2(BufferedImage img, Color col) {
 	BufferedImage ol = outline(img, col);
 	Graphics g = ol.getGraphics();
@@ -1095,7 +1095,7 @@ public class Utils {
 	else
 	    return(a / b);
     }
-    
+
     public static int floormod(int a, int b) {
 	int r = a % b;
 	if(r < 0)
@@ -1116,7 +1116,7 @@ public class Utils {
 	double q = a / b;
 	return((q < 0)?(((int)q) - 1):((int)q));
     }
-    
+
     public static float floormod(float a, float b) {
 	float r = a % b;
 	return((a < 0)?(r + b):r);
@@ -1149,7 +1149,7 @@ public class Utils {
 	    return(max);
 	return(d);
     }
-    
+
     public static float clip(float d, float min, float max) {
 	if(d < min)
 	    return(min);
@@ -1157,7 +1157,7 @@ public class Utils {
 	    return(max);
 	return(d);
     }
-    
+
     public static int clip(int i, int min, int max) {
 	if(i < min)
 	    return(min);
@@ -1233,7 +1233,7 @@ public class Utils {
 			 ((x.getBlue()  * f2) + (y.getBlue()  * f1)) / 255,
 			 ((x.getAlpha() * f2) + (y.getAlpha() * f1)) / 255));
     }
-    
+
     public static Color preblend(Color c1, Color c2) {
 	double a1 = c1.getAlpha() / 255.0;
 	double a2 = c2.getAlpha() / 255.0;
@@ -1251,7 +1251,7 @@ public class Utils {
 	oout.writeObject(obj);
 	oout.flush();
     }
-    
+
     public static byte[] serialize(Object obj) {
 	ByteArrayOutputStream out = new ByteArrayOutputStream();
 	try {
@@ -1261,7 +1261,7 @@ public class Utils {
 	}
 	return(out.toByteArray());
     }
-    
+
     public static Object deserialize(InputStream in) throws IOException {
 	ObjectInputStream oin = new ObjectInputStream(in);
 	try {
@@ -1270,7 +1270,7 @@ public class Utils {
 	    return(null);
 	}
     }
-    
+
     public static Object deserialize(byte[] buf) {
 	if(buf == null)
 	    return(null);
@@ -1281,7 +1281,7 @@ public class Utils {
 	    return(null);
 	}
     }
-    
+
     public static boolean parsebool(String s) {
 	if(s == null)
 	    throw(new IllegalArgumentException(s));
@@ -1303,7 +1303,7 @@ public class Utils {
 	    return(def);
 	}
     }
-    
+
     /* Just in case anyone doubted that Java is stupid. :-/ */
     public static FloatBuffer bufcp(float[] a) {
 	FloatBuffer b = mkfbuf(a.length);
@@ -1406,7 +1406,7 @@ public class Utils {
 		((float)c.getAlpha() / 255.0f)
 	    });
     }
-    
+
     @SuppressWarnings("unchecked")
     public static <T> T[] mkarray(Class<T> cl, int len) {
 	return((T[])Array.newInstance(cl, len));
@@ -1468,7 +1468,7 @@ public class Utils {
     public static <T> T[] extend(T[] src, int nl) {
 	return(extend(src, 0, nl));
     }
-    
+
     public static <T, E extends T> T[] extend(T[] src, E ne) {
 	T[] ret = extend(src, 0, src.length + 1);
 	ret[src.length] = ne;
@@ -1486,25 +1486,25 @@ public class Utils {
 	System.arraycopy(src, 0, dst, 0, Math.min(src.length, dst.length));
 	return(dst);
     }
-    
+
     public static double[] extend(double[] src, int nl) {
 	double[] dst = new double[nl];
 	System.arraycopy(src, 0, dst, 0, Math.min(src.length, dst.length));
 	return(dst);
     }
-    
+
     public static float[] extend(float[] src, int nl) {
 	float[] dst = new float[nl];
 	System.arraycopy(src, 0, dst, 0, Math.min(src.length, dst.length));
 	return(dst);
     }
-    
+
     public static short[] extend(short[] src, int nl) {
 	short[] dst = new short[nl];
 	System.arraycopy(src, 0, dst, 0, Math.min(src.length, dst.length));
 	return(dst);
     }
-    
+
     public static <T> T el(Iterable<T> c) {
 	Iterator<T> i = c.iterator();
 	if(!i.hasNext()) return(null);
