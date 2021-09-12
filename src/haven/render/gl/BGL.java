72d9351ec47b12a78c17df72fa969ddd294abcd2
Fredrik Tolf <fredrik@dolda2000.com>
2021-09-12 23:20:03 +0200

    Optionally use object descriptions to seed GL object labels.
    Turned off by default, since not all GL implementations necessarily support
    it, or might otherwise arbitrarily choke at it for more-or-less unknown
    reasons. For example, my nVidia driver chokes on setting labels between
    glGen* and actually using the object, which I don't exactly think is part
    of the specification.

--- src/haven/render/gl/BGL.java
+++ src/haven/render/gl/BGL.java
@@ -599,6 +599,17 @@ public abstract class BGL {
 	    });
     }
 
+    public void glObjectLabel(final int identifier, final ID name, final int length, final byte[] label) {
+	add(new Command() {
+		public void run(GL3 gl) {gl.glObjectLabel(identifier, name.glid(), length, label, 0);}
+	    });
+    }
+
+    public void glObjectLabel(int identifier, ID name, String label) {
+	byte[] enc = label.getBytes(haven.Utils.utf8);
+	glObjectLabel(identifier, name, enc.length, enc);
+    }
+
     public void glPixelStorei(final int pname, final int param) {
 	add(new Command() {
 		public void run(GL3 gl) {gl.glPixelStorei(pname, param);}
