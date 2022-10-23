a1a0d06deace405d0b0498d6ca9379471ee2e909
Fredrik Tolf <fredrik@dolda2000.com>
2022-10-23 02:50:28 +0200

    Add support for shader uint types.

--- src/haven/render/sl/Cons.java
+++ src/haven/render/sl/Cons.java
@@ -78,6 +78,9 @@ public class Cons {
     public static IVec4Cons ivec4(Expression... els) {return(new IVec4Cons(els));}
     public static IVec3Cons ivec3(Expression... els) {return(new IVec3Cons(els));}
     public static IVec2Cons ivec2(Expression... els) {return(new IVec2Cons(els));}
+    public static UVec4Cons uvec4(Expression... els) {return(new UVec4Cons(els));}
+    public static UVec3Cons uvec3(Expression... els) {return(new UVec3Cons(els));}
+    public static UVec2Cons uvec2(Expression... els) {return(new UVec2Cons(els));}
     public static Mat3Cons mat3(Expression... els) {return(new Mat3Cons(els));}
 
     public static Expression sin(Expression x) {return(Function.Builtin.sin.call(x));}
@@ -130,10 +133,14 @@ public class Cons {
     public static Expression vec2(double a, double b)                     {return(vec2(l(a), l(b)));}
     public static Expression vec3(double a, double b, double c)           {return(vec3(l(a), l(b), l(c)));}
     public static Expression vec4(double a, double b, double c, double d) {return(vec4(l(a), l(b), l(c), l(d)));}
-    public static Expression ivec2(haven.Coord c)    {return(ivec2(l(c.x), l(c.y)));}
+    public static Expression ivec2(haven.Coord c)              {return(ivec2(l(c.x), l(c.y)));}
     public static Expression ivec2(int a, int b)               {return(ivec2(l(a), l(b)));}
     public static Expression ivec3(int a, int b, int c)        {return(ivec3(l(a), l(b), l(c)));}
     public static Expression ivec4(int a, int b, int c, int d) {return(ivec4(l(a), l(b), l(c), l(d)));}
+    public static Expression uvec2(haven.Coord c)              {return(uvec2(l(c.x), l(c.y)));}
+    public static Expression uvec2(int a, int b)               {return(uvec2(l(a), l(b)));}
+    public static Expression uvec3(int a, int b, int c)        {return(uvec3(l(a), l(b), l(c)));}
+    public static Expression uvec4(int a, int b, int c, int d) {return(uvec4(l(a), l(b), l(c), l(d)));}
 
     public static <T> T id(T a) {return(a);}
     public static final UnaryOperator<Expression> idm = in -> in;
