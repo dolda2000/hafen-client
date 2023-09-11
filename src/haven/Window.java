4d31ba132bcc911fdc094aec13714794ffc5395d
Fredrik Tolf <fredrik@dolda2000.com>
2023-09-11 16:47:37 +0200

    Use scaled texture image for window captions.

--- src/haven/Window.java
+++ src/haven/Window.java
@@ -52,7 +52,7 @@ public class Window extends Widget implements DTarget {
     public static final int capo = 7, capio = 2;
     public static final Coord dlmrgn = UI.scale(23, 14);
     public static final Coord dsmrgn = UI.scale(9, 9);
-    public static final BufferedImage ctex = Resource.loadimg("gfx/hud/fonttex");
+    public static final BufferedImage ctex = Resource.loadsimg("gfx/hud/fonttex");
     public static final Text.Furnace cf = new Text.Imager(new PUtils.TexFurn(new Text.Foundry(Text.fraktur, 15).aa(true), ctex)) {
 	    protected BufferedImage proc(Text text) {
 		// return(rasterimg(blurmask2(text.img.getRaster(), 1, 1, Color.BLACK)));
