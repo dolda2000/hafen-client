d6d944537a6e06b44345055616b6c9abfdade38f
Fredrik Tolf <fredrik@dolda2000.com>
2023-09-23 23:07:32 +0200

    Receive initframes directly instead of as part of OD_REMs.

--- src/haven/Connection.java
+++ src/haven/Connection.java
@@ -355,6 +355,8 @@ public class Connection {
 		OCache.ObjDelta delta = new OCache.ObjDelta(fl, id, fr);
 		if((fl & 1) != 0)
 		    delta.initframe = fr;
+		if((fl & 8) != 0)
+		    delta.initframe = msg.int32();
 		while(true) {
 		    int afl = 0, len, type = msg.uint8();
 		    if(type == OCache.OD_END)
@@ -375,12 +377,7 @@ public class Connection {
 		    }
 		    OCache.AttrDelta attr = new OCache.AttrDelta(delta, type, msg, len);
 		    if(type == OCache.OD_REM) {
-			int rfl = attr.eom() ? 0 : attr.uint8();
-			if((rfl & 1) != 0) {
-			    delta.initframe = attr.int32();
-			} else {
-			    delta.rem = true;
-			}
+			delta.rem = true;
 		    } else {
 			delta.attrs.add(attr);
 		    }
