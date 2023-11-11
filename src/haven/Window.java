e7aca64b6cf9c4f5f7e9302a70f1eb84375ace77
Fredrik Tolf <fredrik@dolda2000.com>
2023-11-11 17:50:27 +0100

    Fix window-dragging focus bug.

--- src/haven/Window.java
+++ src/haven/Window.java
@@ -159,7 +159,7 @@ public class Window extends Widget implements DTarget {
 		return(true);
 	    if(checkhit(c)) {
 		Window wnd = (Window)parent;
-		wnd.parent.setfocus(this);
+		wnd.parent.setfocus(wnd);
 		wnd.raise();
 		if(button == 1)
 		    wnd.drag(c);
