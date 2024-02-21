dolda2000/hafen-client

commit 6dde19e76fbc67825a2be08a9fd7e5cb51889abf

Added a hack to make menugrid "shortcuts" from belt kinda-sorta work.

src/haven/GameUI.java
@@ -121,7 +121,12 @@ public class GameUI extends ConsoleHost implements Console.Directory {
 	    }
 	    if(local && (menu != null)) {
 		if(res != null) {
-		    MenuGrid.Pagina pag = menu.paginafor(slot.res);
+		    MenuGrid.Pagina pag;
+		    /* XXX: This is a hack. The pagina system needs to be remade. */
+		    if(res != null)
+			pag = menu.paginafor(res.indir());
+		    else
+			pag = menu.paginafor(slot.res);
 		    try {
 			MenuGrid.PagButton btn = pag.button();
 			menu.use(btn, iact, false);

commit d1e55b312a731ec37d941c989aea37eab7b5f428

Fix local pagbutton belt-activation bug.
It is strange to imagine that I didn't discover this until now, but it
seems this must have been caused by 6dde19e7.

src/haven/GameUI.java
@@ -125,12 +125,10 @@ public class GameUI extends ConsoleHost implements Console.Directory, UI.Message
 	    }
 	    if(local && (menu != null)) {
 		if(res != null) {
-		    MenuGrid.Pagina pag;
-		    /* XXX: This is a hack. The pagina system needs to be remade. */
-		    if(res != null)
+		    MenuGrid.Pagina pag = menu.paginafor(this.res);
+		    /* XXX: This is quite a hack. Is there a better way? */
+		    if(!menu.paginae.contains(pag) && (res != null))
 			pag = menu.paginafor(res.indir());
-		    else
-			pag = menu.paginafor(this.res);
 		    try {
 			MenuGrid.PagButton btn = pag.button();
 			menu.use(btn, iact, false);