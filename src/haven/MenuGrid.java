f837d36ca048a05ad4d34e1289be8e8e0d53d029
Fredrik Tolf <fredrik@dolda2000.com>
2024-02-21 17:07:05 +0100

    Don't crash on pagina misuse.

--- src/haven/MenuGrid.java
+++ src/haven/MenuGrid.java
@@ -173,10 +173,14 @@ public class MenuGrid extends Widget implements KeyBinding.Bindable {
 		    eact = Utils.extend(eact, iact.click.clickargs());
 	    }
 	    AButton act = act();
-	    if((act != null) && (act.ad.length > 0))
+	    if((act != null) && (act.ad.length > 0)) {
 		pag.scm.wdgmsg("act", Utils.extend(Utils.extend(new Object[0], act().ad), eact));
-	    else
-		pag.scm.wdgmsg("use", Utils.extend(new Object[] {pag.id}, eact));
+	    } else {
+		if(pag.id instanceof Indir)
+		    Warning.warn("tried to use resource-indexed pagina by id: " + res);
+		else
+		    pag.scm.wdgmsg("use", Utils.extend(new Object[] {pag.id}, eact));
+	    }
 	}
 	public void tick(double dt) {
 	    if(spr != null)
