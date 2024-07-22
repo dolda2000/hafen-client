8977736730e5d90964258467931270b762318349
Fredrik Tolf <fredrik@dolda2000.com>
2024-07-22 23:53:35 +0200

    Autoselect a value for lambda-based dropboxes.

--- src/haven/SDropBox.java
+++ src/haven/SDropBox.java
@@ -172,6 +172,7 @@ public abstract class SDropBox<I, W extends Widget> extends SListWidget<I, W> {
 
     public static <I> SDropBox<I, Widget> of(int w, int listh, int itemh, List<? extends I>  items, BiFunction<? super I, ? super Coord, ? extends Widget> render, Consumer<? super I> change) {
 	return(new SDropBox<I, Widget>(w, listh, itemh) {
+		{super.change(items.get(0));}
 		protected List<? extends I> items() {return(items);}
 		protected Widget makeitem(I item, int idx, Coord sz) {return(render.apply(item, sz));}
 		public void change(I item) {
