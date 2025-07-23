f2d5de0f027fdf4bc0c02c17e9f3f0bdada063bb
Fredrik Tolf <fredrik@dolda2000.com>
2025-07-23 17:36:21 +0200

    Remove SSLSocket connect functions for SslHelper.
    The argument being that all new potential users of these functions should
    be using NIO instead.

--- src/haven/SslHelper.java
+++ src/haven/SslHelper.java
@@ -163,28 +163,6 @@ public class SslHelper {
 	    };
     }
 
-    public SSLSocket connect(Socket sk, String host, int port, boolean autoclose) throws IOException {
-	return((SSLSocket)sfac().createSocket(sk, host, port, autoclose));
-    }
-
-    public SSLSocket connect(String host, int port) throws IOException {
-	IOException lerr = null;
-	for(InetAddress haddr : InetAddress.getAllByName(host)) {
-	    try {
-		Socket sk = new HackSocket();
-		sk.connect(new InetSocketAddress(haddr, port), 5000);
-		return(connect(sk, host, port, true));
-	    } catch(IOException e) {
-		if(lerr != null)
-		    e.addSuppressed(lerr);
-		lerr = e;
-	    }
-	}
-	if(lerr != null)
-	    throw(lerr);
-	throw(new UnknownHostException(host));
-    }
-
     public boolean hasCreds() {
 	return(creds != null);
     }
