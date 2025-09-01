2276fd30292d65df24c87c49d6b8241653eabc12
Fredrik Tolf <fredrik@dolda2000.com>
2025-09-01 23:53:48 +0200

    Add ILabel widget.

---
+++ src/haven/ILabel.java
@@ -0,0 +1,68 @@
+/*
+ *  This file is part of the Haven & Hearth game client.
+ *  Copyright (C) 2009 Fredrik Tolf <fredrik@dolda2000.com>, and
+ *                     Björn Johannessen <johannessen.bjorn@gmail.com>
+ *
+ *  Redistribution and/or modification of this file is subject to the
+ *  terms of the GNU Lesser General Public License, version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  Other parts of this source tree adhere to other copying
+ *  rights. Please see the file `COPYING' in the root directory of the
+ *  source tree for details.
+ *
+ *  A copy the GNU Lesser General Public License is distributed along
+ *  with the source tree of which this file is a part in the file
+ *  `doc/LPGL-3'. If it is missing for any reason, please see the Free
+ *  Software Foundation's website at <http://www.fsf.org/>, or write
+ *  to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *  Boston, MA 02111-1307 USA
+ */
+
+package haven;
+
+public class ILabel extends Widget {
+    public final Text.Furnace f;
+    public Text text;
+
+    public ILabel(String text, Text.Furnace f) {
+	super(Coord.z);
+	this.f = f;
+	this.text = f.render(text);
+	resize(this.text.sz());
+    }
+
+    public void draw(GOut g) {
+	g.image(text.tex(), Coord.z);
+    }
+
+    public String text() {
+	return(text.text);
+    }
+
+    public void settext(String text) {
+	if(text.equals(this.text.text))
+	    return;
+	this.text.dispose();
+	this.text = f.render(text);
+	resize(this.text.sz());
+    }
+
+    public void dispose() {
+	super.dispose();
+	this.text.dispose();
+    }
+
+    public void uimsg(String msg, Object... args) {
+	if(msg == "set") {
+	    settext((String)args[0]);
+	} else {
+	    super.uimsg(msg, args);
+	}
+    }
+}
