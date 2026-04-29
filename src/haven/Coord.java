844dc4322254c909e7760b02bf362aaa4204e648
Fredrik Tolf <fredrik@dolda2000.com>
2026-04-30 00:04:45 +0200

    Add corner-based Coord.isect.

--- src/haven/Coord.java
+++ src/haven/Coord.java
@@ -152,6 +152,10 @@ public class Coord implements Comparable<Coord>, java.io.Serializable {
 	return(of(Utils.floormod(x, d.x), Utils.floormod(y, d.y)));
     }
 
+    public boolean isect2(Coord ul, Coord br) {
+	return((x >= ul.x) && (y >= ul.y) && (x < br.x) && (y < br.y));
+    }
+
     public boolean isect(Coord c, Coord s) {
 	return((x >= c.x) && (y >= c.y) && (x < c.x + s.x) && (y < c.y + s.y));
     }
